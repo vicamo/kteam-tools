221c286a89ea0495a52c546ba4e25744e569673d
Tim Gardner <tim.gardner@canonical.com>
2013-04-25 14:06:56 -0600

    update-repos.sh: Added saucy and signed repos
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- kernel-team-accounts/update-repos.sh
+++ kernel-team-accounts/update-repos.sh
@@ -4,10 +4,11 @@ LINUX=git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
 LLREPO=linux-2.6.git
 LREPO=linux.git
 UBUNTU=git://kernel.ubuntu.com/ubuntu
-RELEASES="hardy lucid oneiric precise quantal raring"
+RELEASES="hardy lucid oneiric precise quantal raring saucy"
 EXTRAS="ubuntu-hardy-lbm ubuntu-hardy-lrm ubuntu-hardy-lum ubuntu-lucid-lbm ubuntu-oneiric-lbm ubuntu-precise-lbm ubuntu-quantal-lbm"
-EXTRAS="$EXTRAS linux-firmware wireless-crda kernel-testing autotest instrument-lib"
-METAS="hardy lucid oneiric precise quantal"
+EXTRAS="$EXTRAS linux-firmware wireless-crda kernel-testing autotest autotest-client-tests instrument-lib"
+METAS="hardy lucid oneiric precise quantal raring saucy"
+SIGNED="precise quantal raring saucy"
 LOCK=/tmp/update-repos.lock
 ORIGS="2.6.24 2.6.32 2.6.38 3.0.0 3.2.0 3.5.0 3.8.0"
 
@@ -52,6 +53,16 @@ do
 		git clone ${UBUNTU}/ubuntu-${i}-meta.git ubuntu-${i}-meta.git
 	else
 		(cd ubuntu-${i}-meta.git;git fetch origin;git fetch origin master;git reset --hard FETCH_HEAD)
+	fi
+done
+
+for i in ${SIGNED}
+do
+	if [ ! -d ubuntu-${i}-signed.git ]
+	then
+		git clone ${UBUNTU}/ubuntu-${i}-signed.git ubuntu-${i}-signed.git
+	else
+		(cd ubuntu-${i}-signed.git;git fetch origin;git fetch origin master;git reset --hard FETCH_HEAD)
 	fi
 done
 
