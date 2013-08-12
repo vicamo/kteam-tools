3bef05492daec88a7c2ac4cfb7e72f99d8026566
Tim Gardner <tim.gardner@canonical.com>
2013-08-12 09:22:50 -0600

    stable: Update 13.10 kernel version to 3.11
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- ktl/ubuntu.py
+++ ktl/ubuntu.py
@@ -134,7 +134,7 @@ class Ubuntu:
         {
             'development' : True,        # This is the version that is currently under development
             'series_version' : '13.10',
-            'kernel'    : '3.10.0',
+            'kernel'    : '3.11.0',
             'name'      : 'saucy',
             'supported' : False,
             # adjust packages when this goes live
