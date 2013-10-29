a4835ed585171ebd735b49a67030f19e7cf85081
Andy Whitcroft <apw@canonical.com>
2013-10-29 16:20:56 -0700

    zinc: add virgin repo update script
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

---
+++ kernel-team-accounts/update-repos-zinc.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+CWD=/srv/kernel.ubuntu.com/git/virgin
+LINUX=git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
+LREPO=linux.git
+LOCK=/tmp/update-repos.lock
+
+if [ ! "$1" = "" ]
+then
+	CWD="$1"
+fi
+
+cd $CWD || exit 1
+
+if [ -f $LOCK ]
+then
+	exit 1
+fi
+echo 1 > $LOCK
+
+if [ ! -d ${LREPO} ]
+then
+	git clone --bare ${LINUX} ${LREPO}
+	(cd ${LREPO}; git fetch origin)
+fi
+
+rm -f $LOCK
