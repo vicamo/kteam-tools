4961d2de9252903a340941146bd7c1c9dc67b8af
Andy Whitcroft <apw@canonical.com>
2014-07-11 20:58:10 +0100

    shanky: add linux-keystone (cough) to components list for main
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- ktl/check_component.py
+++ ktl/check_component.py
@@ -161,7 +161,7 @@ class CheckComponent():
             package.startswith('linux-meta-lts-') or
             package.startswith('linux-signed-lts-')):
             return self.main_component
-        if package in ['linux-ec2', 'linux-ti-omap4', 'linux-armadaxp', 'linux-touchstone']:
+        if package in ['linux-ec2', 'linux-ti-omap4', 'linux-armadaxp', 'linux-keystone']:
             return self.main_component
         return self.default_component
 
