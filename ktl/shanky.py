3d7ee852d3edfe5c4dfe96f07e9d57950d27807a
Brad Figg <brad.figg@canonical.com>
2014-09-02 14:15:18 -0700

    [ktl/shanky.py] Create a common module for writing to shankbot's pipe.
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

---
+++ ktl/shanky.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#
+
+# The named pipe used for getting messages to shankbot, the irc bot.
+#
+shank_pipe_path = "/tmp/shank.pipe"
+
+def send_to_shankbot(msg):
+    with open(shank_pipe_path, 'w') as shank_pipe:
+        shank_pipe.write(msg)
+
+# vi:set ts=4 sw=4 expandtab:
