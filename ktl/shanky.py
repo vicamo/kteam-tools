2674b173fb0b8d225b051ca68811931d9004f847
Andy Whitcroft <apw@canonical.com>
2014-09-04 16:18:51 +0100

    ktl: allow shanky.py to be used as an executable
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- ktl/shanky.py
+++ ktl/shanky.py
@@ -9,4 +9,9 @@ def send_to_shankbot(msg):
     with open(shank_pipe_path, 'w') as shank_pipe:
         shank_pipe.write(msg)
 
+
+if __name__ == "__main__":
+    import sys
+    send_to_shankbot(' '.join(sys.argv[1:]) + '\n')
+
 # vi:set ts=4 sw=4 expandtab:
