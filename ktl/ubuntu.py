fc7142698ca2a85e681e9834d5082187e863e9b9
Andy Whitcroft <apw@canonical.com>
2015-02-27 09:35:25 +0000

    ktl: vivid is now 3.19.0
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- ktl/ubuntu.py
+++ ktl/ubuntu.py
@@ -135,7 +135,7 @@ class Ubuntu:
         {
             'development' : True,        # This is the version that is currently under development
             'series_version' : '15.04',
-            'kernel'    : '3.18.0',
+            'kernel'    : '3.19.0',
             'name'      : 'vivid',
             'supported' : False,
             # adjust packages when this goes live
