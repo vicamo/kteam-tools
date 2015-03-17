eb68eb3306abc9239c3af787ae67c1fd466f6b81
Andy Whitcroft <apw@canonical.com>
2015-03-17 10:25:13 +0000

    kmsqg: allow non-persistant queues
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- ktl/msgq.py
+++ ktl/msgq.py
@@ -16,15 +16,20 @@ class MsgQueue():
         s.channel = connection.channel()
         s.channel.exchange_declare(exchange=s.exchange_name, type='topic')
 
-    def listen(s, queue_name, routing_key, handler_function):
+    def listen(s, queue_name, routing_key, handler_function, queue_durable=True):
         def wrapped_handler(channel, method, properties, body):
             payload = json.loads(body)
             handler_function(payload)
 
-        s.channel.queue_declare(queue_name, durable=True)
+        s.channel.queue_declare(queue_name, durable=queue_durable)
         s.channel.queue_bind(exchange=s.exchange_name, queue=queue_name, routing_key=routing_key)
         s.channel.basic_consume(wrapped_handler, queue=queue_name, no_ack=True)
         s.channel.start_consuming()
+
+
+    def queue_delete(s, queue_name):
+        s.channel.queue_delete(queue_name)
+
 
     def publish(s, routing_key, payload):
         message_body = json.dumps(payload)
