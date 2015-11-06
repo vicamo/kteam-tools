c414b73c8058e656c27626581ccc1676f6675bdd
Andy Whitcroft <apw@canonical.com>
2015-11-06 20:02:39 +0000

    update-repos: add xenial
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- kernel-team-accounts/update-repos2.sh
+++ kernel-team-accounts/update-repos2.sh
@@ -72,7 +72,7 @@ ubuntu-wily.git			git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+g
 ubuntu-wily-meta.git		git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux-meta/+git/wily	-
 ubuntu-wily-signed.git		git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux-signed/+git/wily	-
 ubuntu-precise-lbm.git		git://kernel.ubuntu.com/ubuntu/ubuntu-precise-lbm.git				-
-ubuntu-wily.git			git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/wily		linux.git
+ubuntu-xenial.git		git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/xenial		linux.git
 unstable.git			git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/unstable	linux.git
 linux-firmware.git		git://kernel.ubuntu.com/ubuntu/linux-firmware.git				-
 wireless-crda.git		git://kernel.ubuntu.com/ubuntu/wireless-crda.git				-
