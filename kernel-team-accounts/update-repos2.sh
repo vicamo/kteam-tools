e1ea3a7dd9aabefdafc6101fef71c087f66023ad
Tim Gardner <tim.gardner@canonical.com>
2015-12-03 09:58:54 -0700

    update-repos2.sh: Use info/repositories.txt
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- kernel-team-accounts/update-repos2.sh
+++ kernel-team-accounts/update-repos2.sh
@@ -3,6 +3,7 @@
 CWD=/usr3/ubuntu
 LOCK=/tmp/update-repos.lock
 
+SD=`pwd`
 if [ ! "$1" = "" ]
 then
 	CWD="$1"
@@ -23,12 +24,12 @@ do
 	file=`basename "$url"`
 
 	if [ ! -f "$file" ]; then
-		wget "$url"
+		echo Getting "$url"
+		wget -q "$url"
 	fi
 done <<EOL
 http://archive.ubuntu.com/ubuntu/pool/main/l/linux/linux_3.2.0.orig.tar.gz
 http://archive.ubuntu.com/ubuntu/pool/main/l/linux/linux_3.13.0.orig.tar.gz
-http://archive.ubuntu.com/ubuntu/pool/main/l/linux/linux_3.16.0.orig.tar.gz
 http://archive.ubuntu.com/ubuntu/pool/main/l/linux/linux_3.19.0.orig.tar.gz
 http://archive.ubuntu.com/ubuntu/pool/main/l/linux/linux_4.2.0.orig.tar.gz
 http://archive.ubuntu.com/ubuntu/pool/main/l/linux-lts-trusty/linux-lts-trusty_3.13.0.orig.tar.gz
@@ -37,7 +38,7 @@ http://archive.ubuntu.com/ubuntu/pool/main/l/linux-lts-vivid/linux-lts-vivid_3.1
 http://archive.ubuntu.com/ubuntu/pool/main/l/linux-lts-wily/linux-lts-wily_4.2.0.orig.tar.gz
 EOL
 
-# Clone and update all our current repos.
+# Get a couple of special repos first
 while read repo url ref
 do
 	case "$repo" in
@@ -57,33 +58,36 @@ do
 			[ -d .git ] && git checkout -qf)
 	fi
 done <<EOL
-linux.git			git://kernel.ubuntu.com/virgin/linux.git
-ubuntu-precise.git		git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/precise	linux.git
-ubuntu-precise-meta.git		git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux-meta/+git/precise	-
-ubuntu-precise-signed.git	git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux-signed/+git/precise	-
-ubuntu-trusty.git		git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/trusty		linux.git
-ubuntu-trusty-meta.git		git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux-meta/+git/trusty	-
-ubuntu-trusty-signed.git	git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux-signed/+git/trusty	-
-ubuntu-utopic.git		git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/utopic		linux.git
-ubuntu-utopic-meta.git		git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux-meta/+git/utopic	-
-ubuntu-utopic-signed.git	git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux-signed/+git/utopic	-
-ubuntu-vivid.git		git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/vivid		linux.git
-ubuntu-vivid-meta.git		git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux-meta/+git/vivid	-
-ubuntu-vivid-signed.git		git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux-signed/+git/vivid	-
-ubuntu-wily.git			git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/wily		linux.git
-ubuntu-wily-meta.git		git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux-meta/+git/wily	-
-ubuntu-wily-signed.git		git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux-signed/+git/wily	-
-ubuntu-precise-lbm.git		git://kernel.ubuntu.com/ubuntu/ubuntu-precise-lbm.git				-
-ubuntu-xenial.git		git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/xenial		linux.git
-unstable.git			git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/unstable	linux.git
-linux-firmware.git		git://kernel.ubuntu.com/ubuntu/linux-firmware.git				-
-wireless-crda.git		git://kernel.ubuntu.com/ubuntu/wireless-crda.git				-
-kernel-testing.git		git://kernel.ubuntu.com/ubuntu/kernel-testing.git				-
-autotest.git			git://kernel.ubuntu.com/ubuntu/autotest.git					-
-autotest-client-tests.git	git://kernel.ubuntu.com/ubuntu/autotest-client-tests.git			-
-instrument-lib.git		git://kernel.ubuntu.com/ubuntu/instrument-lib.git				-
-kteam-tools.git			git://kernel.ubuntu.com/ubuntu/kteam-tools.git					-
+linux.git			git://kernel.ubuntu.com/virgin/linux.git					-
 kteam-tools			git://kernel.ubuntu.com/ubuntu/kteam-tools.git					-
 EOL
+
+cat `dirname $SD/$0`/../info/repositories.txt | while read k_u_c lp master status
+do
+	if [ "$status" = "inactive" ]
+	then
+		continue
+	fi
+
+	case $master in
+		either|launchpad)
+			URL=$lp
+			;;
+		wani)
+			URL=$k_u_c
+			;;
+		*)
+			echo "Unknown master location: $master"
+			exit 1
+			;;
+	esac
+
+	if [ ! -d `basename $URL` ]
+	then
+		git clone --bare --reference linux.git $URL `basename $URL`
+	fi
+
+	(cd `basename $URL`; git fetch --tags origin)
+done
 
 rm -f $LOCK
