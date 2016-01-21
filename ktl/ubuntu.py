9ba1393f7a9701e300269893fa25fee3460bceb5
Tim Gardner <tim.gardner@canonical.com>
2016-01-21 14:57:39 -0700

    ktl/ubuntu.py: xenial is now 4.4
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- ktl/ubuntu.py
+++ ktl/ubuntu.py
@@ -135,7 +135,7 @@ class Ubuntu:
         {
             'development' : True,        # This is the version that is currently under development
             'series_version' : '16.04',
-            'kernel'    : '4.3.0',
+            'kernel'    : '4.4.0',
             'name'      : 'xenial',
             'supported' : False,
             # adjust packages when this goes live
