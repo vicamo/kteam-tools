35f2fc403a5200e8401702704b887e6d67f77b66
Brad Figg <brad.figg@canonical.com>
2016-04-14 11:24:44 -0700

    [launchpad_bug.py] A sopel irc bot module to display the title and url for a launchpad bug based on matching certain text
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

---
+++ sopel-modules/launchpad_bug.py
@@ -0,0 +1,31 @@
+import sopel.module
+import re
+from launchpadlib.launchpad         import Launchpad
+
+# process_matches
+#
+def process_matches(bot, matches):
+    lp = Launchpad.login_anonymously('happy', 'production')
+    for g in matches:
+        for bug_id in g:
+            if bug_id != '':
+                try:
+                    lp_bug = lp.bugs[bug_id]
+                    text = '"%s"     https://launchpad.net/bugs/%s' % (lp_bug.title, bug_id)
+                    bot.say(text)
+                except:
+                    # This must be a private bug
+                    #
+                    text = "LP: #%s may be private (I can't find it in LP)." % bug_id
+                    bot.say(text)
+
+@sopel.module.rule('(?i)(?:bugs?[ /]#?([0-9]+)|(?:^|\W)#([0-9]{5,}))')
+def rule1(bot, trigger):
+    m = re.findall('(?i)(?:bugs?[ /]#?([0-9]+)|(?:^|\W)#([0-9]{5,}))', trigger)
+    process_matches(bot, m)
+
+@sopel.module.rule('\\b[lL][pP]:?\s?#?([0-9]+)(,\s*#?[0-9]+)*\\b')
+def rule2(bot, trigger):
+    m = re.findall('\\b[lL][pP]:?\s?#?([0-9]+)(,\s*#?[0-9]+)*\\b', trigger)
+    process_matches(bot, m)
+
