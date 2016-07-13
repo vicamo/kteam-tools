62adf1f703f2638a548f65527b5c749dc76396b9
Brad Figg <brad.figg@canonical.com>
2016-07-13 09:47:53 -0700

    tracking_bug.py: Refactoring, instrumentation, comments to make it more understandable
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

--- ktl/tracking_bug.py
+++ ktl/tracking_bug.py
@@ -5,52 +5,7 @@ from ktl.workflow                       import Workflow, DefaultAssigneeMissing
 from ktl.ubuntu                         import Ubuntu
 import re
 
-from logging                            import info, debug
-from ktl.termcolor                      import colored
-
-def cinfo(msg, color='white'):
-    Clog.info(msg, color)
-
-def cdebug(msg, color='magenta'):
-    Clog.debug(msg, color)
-
-def cwarn(msg, color='red'):
-    Clog.warn(msg, color)
-
-def cerror(msg, color='red'):
-    Clog.warn(msg, color)
-
-def cnotice(msg, color='yellow'):
-    Clog.notice(msg, color)
-
-class Clog:
-    '''
-    Colored logging.
-    '''
-    dbg = False
-
-    @classmethod
-    def info(c, msg, color='white'):
-        if c.dbg:
-            # I do this becaus i'm weird and like things lined up in my log output
-            # and "INFO -" is fewer chars then "DEBUG -" and so things don't line
-            # up.
-            #
-            debug(colored(msg, color))
-        else:
-            info(colored(msg, color))
-
-    @classmethod
-    def debug(c, msg, color='magenta'):
-        debug(colored(msg, color))
-
-    @classmethod
-    def warn(c, msg, color='red'):
-        c.info(colored(msg, color))
-
-    @classmethod
-    def notice(c, msg, color='yellow'):
-        c.info(colored(msg, color))
+from ktl.log                            import cdebug, cerror, cwarn, center, cleave
 
 class TrackingBug:
 
@@ -92,27 +47,166 @@ class TrackingBug:
             if lp_series.name in ['trunk', 'latest']:
                 break
             if lp_series.name == 'upload-to-ppa' and not series_specified:
-                cdebug('    no upload-to-ppa')
+                cdebug('    no upload-to-ppa', 'yellow')
                 break
             if lp_series.name == 'prepare-package-lbm' and not s.has_dependent_package(targeted_series_name, package, 'lbm'):
-                cdebug('    no prepare-package-lbm')
+                cdebug('    no prepare-package-lbm', 'yellow')
                 break
             if lp_series.name == 'prepare-package-meta' and not s.has_dependent_package(targeted_series_name, package, 'meta'):
-                cdebug('    no prepare-package-meta')
+                cdebug('    no prepare-package-meta', 'yellow')
                 break
             if lp_series.name == 'prepare-package-ports-meta' and not s.has_dependent_package(targeted_series_name, package, 'ports-meta'):
-                cdebug('    no prepare-package-ports-meta')
+                cdebug('    no prepare-package-ports-meta', 'yellow')
                 break
             if lp_series.name == 'prepare-package-signed' and not s.has_dependent_package(targeted_series_name, package, 'signed'):
-                cdebug('    no prepare-package-signed')
+                cdebug('    no prepare-package-signed', 'yellow')
                 break
             retval = True
             break
         return retval
 
-    def open(self, package, version, new_abi, master_bug, series_specified=None):
+    @property
+    def isdev(self):
+        return self.ub.is_development_series(self.targeted_series_name)
 
-        cdebug('open enter')
+    @property
+    def targeted_series_name(self):
+        return self._targeted_series_name
+
+    @property
+    def series_specified(self):
+        return self._series_specified
+
+    @property
+    def series_target(self):
+        return self._series_target
+
+    def reset_tags(self, bug):
+        '''
+        Set the bug up with it's initial set of tags. If this is an existing tracking bug and
+        we are resetting it to the default then we should remove any/all existing tags.
+        '''
+        for tag in bug.tags:
+            bug.tags.remote(tag)
+
+        taglist = self.wf.initial_tags(self.package, self.isdev)
+
+        # Add the 'block-proposed' tag both for development kernels and for the
+        # latest release series
+        if self.isdev or self.ub.last_release == self.targeted_series_name:
+            taglist.append('block-proposed')
+        if not self.isdev:
+            taglist.append('block-proposed-%s' % self.targeted_series_name)
+        for itag in taglist:
+            bug.tags.append(itag)
+
+    def add_subscribers(self, bug):
+        '''
+        Teams / individuals to be automatically subscribed to the tracking bugs.  These vary
+        per package.
+        '''
+        teams = self.wf.subscribers(self.package, self.isdev)
+        for team in teams:
+            try:
+                lp_team = self.lp.launchpad.people[team]
+            except KeyError:
+                if not self.quiet:
+                    print("Can't subscribe '%s', team not found in Launchpad!" % (team))
+                continue
+            bug.lpbug.subscribe(person=lp_team)
+
+    def add_kernel_series_nomination(self, bug):
+        '''
+        Nominate the series for this package.
+        '''
+        nomination = bug.lpbug.addNomination(target=self.series_target)
+        if nomination.canApprove():
+            nomination.approve()
+        bug.tags.append(self.targeted_series_name)
+
+    def reset_tasks(self, bug):
+        center(self.__class__.__name__ + '.reset_tasks')
+
+        # Set task assignments and importance. Main project task must be
+        # set to In Progress for the bot to do its processing.
+        #
+        cdebug("")
+        cdebug("Setting status and importance", 'blue')
+        for task in bug.tasks:
+            task_name       = task.bug_target_display_name
+            cdebug('    %s' % task_name, 'cyan')
+            parts = task_name.partition(self.lp_project.display_name)
+
+            # Is this the main linux task? "linux (Ubuntu)"
+            #
+            if not self.isdev and 'linux' in parts[0] and self.targeted_series_name.capitalize() not in parts[0]:
+                task.status = "Invalid"
+                cdebug('        is main linux task', 'white')
+                cdebug('        status: %s; importance: %s' % (task.status, task.importance), 'green')
+
+            # Is this the main SRU Workflow task? "Kernel SRU Workflow"
+            #
+            elif parts[0] == '' and parts[1] == self.lp_project.display_name and parts[2] == '':
+                cdebug('        is main SRU Workflow task', 'white')
+                task.status = "In Progress"
+                task.importance = "Medium"
+                cdebug('        status: %s; importance: %s' % (task.status, task.importance))
+
+            # Else, it must be one of the SRU Workflow tasks.
+            #
+            else:
+                cdebug('        is a SRU Workflow task', 'white')
+                if parts[0] != '':
+                    # The linux series task?
+                    #
+                    try:
+                        task.importance = "Medium"
+                    except:
+                        cwarn('Failed to set the task (%s) importance to "Medium".' % (task_name))
+                    cdebug('        status: %s; importance: %s' % (task.status, task.importance))
+                    continue # We don't set the assignee for this task
+
+                task.importance = "Medium"      # All tasks are Medium importance
+
+                # Determine and set the assignee.
+                #
+                task_name = parts[2].strip()
+                try:
+                    assignee = self.wf.assignee(self.package, task_name, self.isdev)
+                except DefaultAssigneeMissing as e:
+                    print('*** Warning:')
+                    print('    %s' % e.message)
+                    continue
+                if assignee is None:
+                    if not self.quiet:
+                        print('Note: Found a workflow task named %s with no automatic assignee, leaving unassigned and setting to invalid' % task_name)
+                    task.status = "Invalid"
+                    cdebug('        status: Invalid')
+                else:
+                    try:
+                        task.assignee = self.lp.launchpad.people[assignee]
+                        cdebug('        assigning: %s' % task.assignee.display_name)
+                    except:
+                        if not self.quiet:
+                            print("Can't assign '%s', team not found in Launchpad!" % (assignee))
+
+                # Determine and mark appropriate tasks Invalid
+                #
+                lin_ver = re.findall('([0-9]+\.[^-]+)', self.version)
+                if lin_ver:
+                    lin_ver = lin_ver[0]
+                    if not self.isdev and self.wf.is_task_invalid(self.package, task_name, lin_ver):
+                        task.status = "Invalid"
+                        cdebug('        status: Invalid')
+                        continue
+                if not self.new_abi and task_name.startswith('prepare-package-') and task_name != 'prepare-package-signed':
+                    task.status = "Invalid"
+                    cdebug('        status: Invalid')
+
+        cleave(self.__class__.__name__ + '.reset_tasks')
+
+    def open(self, package, version, new_abi, master_bug, series_specified=None):
+        center(self.__class__.__name__ + '.open')
         cdebug('    package: %s' % package)
         cdebug('    version: %s' % version)
         cdebug('    new_abi: %s' % new_abi)
@@ -121,6 +215,10 @@ class TrackingBug:
 
         self.__dependency_list = None
 
+        self.package = package
+        self.version = version
+        self.new_abi = new_abi
+
         # For the given version, figure out the series.
         # If we can't find the series, don't continue.
         #
@@ -128,18 +226,22 @@ class TrackingBug:
         targeted_series_name = series_specified
         if not series_specified:
             targeted_series_name = self.ub.series_name(package, version)
+            cdebug('targeted_series_name: %s' % targeted_series_name)
         if targeted_series_name:
             lp = self.lp.launchpad
             ubuntu = lp.distributions["ubuntu"]
             sc = ubuntu.series_collection
             for s in sc:
+                cdebug('s.name: %s' % s.name)
                 if s.name == targeted_series_name:
                     series_target = s
+                    self._series_target = series_target
                     break
         if not series_target:
-            raise Exception("%s-%s: can't figure out the distro series for it."
-                            % (package, version))
-        devel_series = self.ub.is_development_series(targeted_series_name)
+            raise Exception("%s-%s: can't figure out the distro series for it." % (package, version))
+
+        self._targeted_series_name = targeted_series_name
+        self._series_specified = series_specified
 
         # Title: <package>: <version> -proposed tracker
         title = "%s: %s -proposed tracker" % (package, version)
@@ -156,19 +258,21 @@ class TrackingBug:
         description += " https://wiki.ubuntu.com/Kernel/kernel-sru-workflow\n"
 
         prop_pfx = 'kernel'
-        if not devel_series:
+        if not self.isdev:
             prop_pfx += '-stable'
 
         if master_bug:
             description += '-- swm properties --\n'
             description += '%s-master-bug: %s' % (prop_pfx, master_bug)
 
+        cdebug("")
+        cdebug("Creating the bug", 'blue')
         try:
             bug = self.lp.create_bug(project='ubuntu', package=package, title=title, description=description)
         except:
             cerror('Bug creation failed: project: "ubuntu", package: %s' % package)
             cerror('                     (It\'s possible the package does not exist)')
-            cdebug('open leave')
+            cleave(self.__class__.__name__ + '.open')
             return None
 
         id = bug.id
@@ -178,37 +282,9 @@ class TrackingBug:
             else:
                 print("https://bugs.launchpad.net/bugs/%s" % (id))
 
-        # Tags:
-        #    add all tags for this package name
-        taglist = self.wf.initial_tags(package, devel_series)
-        # Add the 'block-proposed' tag both for development kernels and for the
-        # latest release series
-        if devel_series or self.ub.last_release == targeted_series_name:
-            taglist.append('block-proposed')
-        if not devel_series:
-            taglist.append('block-proposed-%s' % targeted_series_name)
-        for itag in taglist:
-            bug.tags.append(itag)
-
-        # Teams / individuals to be automatically subscribed to the tracking bugs
-        #   These vary per package
-        #
-        teams = self.wf.subscribers(package, devel_series)
-        for team in teams:
-            try:
-                lp_team = self.lp.launchpad.people[team]
-            except KeyError:
-                if not self.quiet:
-                    print("Can't subscribe '%s', team not found in Launchpad!" % (team))
-                continue
-            bug.lpbug.subscribe(person=lp_team)
-
-        # Nominate the series for this package.
-        #
-        nomination = bug.lpbug.addNomination(target=series_target)
-        if nomination.canApprove():
-            nomination.approve()
-        bug.tags.append(targeted_series_name)
+        self.reset_tags(bug)         # Add all the tags to the bug
+        self.add_subscribers(bug)
+        self.add_kernel_series_nomination(bug)
 
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         # Add a task for kernel-sru-workflow and then nominate all the series that belong
@@ -216,73 +292,27 @@ class TrackingBug:
         #
 
         lp = self.lp.launchpad
-        if devel_series:
+        if self.isdev:
             project = 'kernel-development-workflow'
         else:
             project = 'kernel-sru-workflow'
 
-        lp_project = lp.projects[project]
-        bug.lpbug.addTask(target=lp_project)
+        cdebug("")
+        cdebug("Adding %s task." % project, 'blue')
+        self.lp_project = lp.projects[project]
+        bug.lpbug.addTask(target=self.lp_project)
 
-        sc = lp_project.series_collection
+        sc = self.lp_project.series_collection
         for series in sc:
             if self.valid_series(series, series_specified, targeted_series_name, package):
-                cdebug('    adding nomination: %s' % series)
+                cdebug('    adding: %s' % series.display_name)
                 nomination = bug.lpbug.addNomination(target=series)
                 if nomination.canApprove():
                     nomination.approve()
 
-        # Set task assignments and importance. Main project task must be
-        # set to In Progress for the bot to do its processing.
-        #
-        for task in bug.tasks:
-            task_name       = task.bug_target_display_name
-            cdebug('    task: %s' % task_name)
-            parts = task_name.partition(lp_project.display_name)
-            if not devel_series and 'linux' in parts[0] and targeted_series_name.capitalize() not in parts[0]:
-                task.status = "Invalid"
-                cdebug('        status: %s; importance: %s' % (task.status, task.importance), 'green')
-            elif parts[0] == '' and parts[1] == lp_project.display_name and parts[2] == '':
-                task.status = "In Progress"
-                task.importance = "Medium"
-                cdebug('        status: %s; importance: %s' % (task.status, task.importance))
-            else:
-                if parts[0] != '':
-                    try:
-                        task.importance = "Medium"
-                    except:
-                        cwarn('Failed to set the task (%s) importance to "Medium".' % (task_name))
-                    cdebug('        status: %s; importance: %s' % (task.status, task.importance))
-                    continue
-                task.importance = "Medium"
-                task_name = parts[2].strip()
-                try:
-                    assignee = self.wf.assignee(package, task_name, devel_series)
-                except DefaultAssigneeMissing as e:
-                    print('*** Warning:')
-                    print('    %s' % e.message)
-                    continue
-                if assignee is None:
-                    if not self.quiet:
-                        print('Note: Found a workflow task named %s with no automatic assignee, leaving unassigned and setting to invalid' % task_name)
-                    task.status = "Invalid"
-                else:
-                    try:
-                        task.assignee = self.lp.launchpad.people[assignee]
-                    except:
-                        if not self.quiet:
-                            print("Can't assign '%s', team not found in Launchpad!" % (assignee))
-                    lin_ver = re.findall('([0-9]+\.[^-]+)', version)
-                    if lin_ver:
-                        lin_ver = lin_ver[0]
-                        if not devel_series and self.wf.is_task_invalid(package, task_name, lin_ver):
-                            task.status = "Invalid"
-                            continue
-                    if not new_abi and task_name.startswith('prepare-package-'):
-                        if task_name != 'prepare-package-signed':
-                            task.status = "Invalid"
+        self.reset_tasks(bug)
 
-        cdebug('open leave')
+        cleave(self.__class__.__name__ + '.open')
         return bug
 
 # vi:set ts=4 sw=4 expandtab:
