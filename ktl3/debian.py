4d8c7fdba17b2ddbe0dba5eb6e29ddb35074e3ee
Brad Figg <brad.figg@canonical.com>
2017-02-22 11:29:41 -0800

    ktl3/git.py, ktl3/debian.py python3 versions of our helpers
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

---
+++ ktl3/debian.py
@@ -0,0 +1,270 @@
+#!/usr/bin/env python
+#
+
+# Note: It would be nice to not tie the debian class to the git class but
+#       be able to handle projects that are under bzr as well. But for
+#       now, we need to be practical for now.
+
+from sys                                import stdout
+from .git                               import Git, GitError
+from re                                 import compile, findall, finditer
+from os                                 import path, listdir
+
+# stdo
+#
+# My own version of print but won't automatically add a linefeed to the end. And
+# does a flush after every write.
+#
+def stdo(ostr):
+    stdout.write(ostr)
+    stdout.flush()
+    return
+
+# debug
+#
+# Print strings to standard out preceeded by "debug:".
+#
+def debug(out, dbg=False, prefix=True):
+    if dbg:
+        if prefix:
+            stdo("debug: ")
+        stdo(out)
+
+# DebianError
+#
+class DebianError(Exception):
+    # __init__
+    #
+    def __init__(self, error):
+        self.msg = error
+
+# Note: Think about cacheing the decoded, changelog dictionary.
+#
+
+class Debian:
+    verbose = False
+    debug = False
+    version_line_rc = compile("^(linux[-\S]*) \(([0-9]+\.[0-9]+\.[0-9]+[-\.][0-9]+\.[0-9]+[~\S]*)\) (\S+); urgency=\S+$")
+    version_rc      = compile("^([0-9]+\.[0-9]+\.[0-9]+)[-\.]([0-9]+)\.([0-9]+)([~\S]*)$")
+
+    package_rc = compile("^(linux[-\S])*.*$")
+    ver_rc     = compile("^linux[-\S]* \(([0-9]+\.[0-9]+\.[0-9]+[-\.][0-9]+\.[0-9]+[~a-z0-9]*)\).*$")
+    bug_rc = compile("LP:\s*#[0-9]+(?:\s*,\s*#[0-9]+)*")
+    bug_nr_rc = compile("#([0-9]+)")
+
+    # debian_directories
+    #
+    @classmethod
+    def debian_directories(cls):
+        # Find the correct debian directory for this branch of this repository.
+        #
+        current_branch = Git.current_branch()
+
+        # If we have a debian/debian.env then open and extract the DEBIAN=...
+        # location.
+        debug("Checking debian/debian.env", cls.debug)
+        debdirs = []
+        try:
+            debian_env = Git.show("debian/debian.env", branch=current_branch)
+            for line in debian_env:
+                if 'DEBIAN=' in line:
+                    (var, val) = line.split('=', 1)
+                    val = val.rstrip()
+
+                    if var == 'DEBIAN':
+                        debdirs.append(val)
+                        break
+            debug("SUCCEEDED\n", cls.debug, False)
+        except GitError:
+            debug("FAILED\n", cls.debug, False)
+            debdirs += ['debian', 'meta-source/debian']
+
+        return debdirs
+
+    # master_changelog
+    #
+    @classmethod
+    def master_changelog(cls):
+        '''
+        The 'changelog' method returns the changelog related to the current branch. This
+        method always returns the changelog from the debian.master directory.
+        '''
+        fid = 'debian.master/changelog'
+        if path.exists(fid):
+            with open(fid, 'r') as f:
+                changelog_contents = f.read()
+
+            retval = cls.changelog_as_list(changelog_contents.split('\n'))
+        else:
+            raise DebianError('Failed to find the master changelog.')
+        return retval
+
+    # raw_changelog
+    #
+    @classmethod
+    def raw_changelog(cls, local=False):
+
+        # Find the correct changelog for this branch of this repository.
+        #
+        current_branch = Git.current_branch()
+
+        # Check each possible directory for a changelog
+        debian_dirs = cls.debian_directories()
+        for debdir in debian_dirs:
+            chglog = debdir + '/changelog'
+            debug("Trying '%s': " % chglog, cls.debug)
+            try:
+                if local:
+                    changelog_contents = open(chglog, 'r').read().split('\n')
+                else:
+                    changelog_contents = Git.show(chglog, branch=current_branch)
+                return changelog_contents, chglog
+            except (GitError, OSError, IOError):
+                debug("FAILED\n", cls.debug, False)
+
+        # Not there anywhere, barf
+        raise DebianError('Failed to find the changelog.')
+
+    # changelog
+    #
+    @classmethod
+    def changelog(cls, local=False):
+        changelog_contents, changelog_path = cls.raw_changelog(local)
+        return cls.changelog_as_list(changelog_contents)
+
+    # changelog_as_list
+    #
+    @classmethod
+    def changelog_as_list(cls, changelog_contents):
+        retval = []
+
+        # The first line of the changelog should always be a version line.
+        #
+        m = cls.version_line_rc.match(changelog_contents[0])
+        if m is None:
+            if cls.debug:
+                m = cls.package_rc.match(changelog_contents[0])
+                if m is None:
+                    debug('The package does not appear to be in a recognized format.\n', cls.debug)
+
+                m = cls.ver_rc.match(changelog_contents[0])
+                if m is None:
+                    debug('The version does not appear to be in a recognized format.\n', cls.debug)
+
+            raise DebianError("The first line in the changelog is not a version line.")
+
+        content = []
+        bugs = []
+
+        for line in changelog_contents:
+            m = cls.version_line_rc.match(line)
+            if m is not None:
+                version = ""
+                release = ""
+                pocket  = ""
+                package = m.group(1)
+                version = m.group(2)
+                rp = m.group(3)
+                if '-' in rp:
+                    release, pocket = rp.split('-')
+                else:
+                    release = rp
+
+                section = {}
+                section['version'] = version
+                section['release'] = release
+                section['series']  = release
+                section['pocket']  = pocket
+                section['content'] = content
+                section['package'] = package
+                section['bugs'] = set(bugs)
+
+                m = cls.version_rc.match(version)
+                if m is not None:
+                    section['linux-version'] = m.group(1)
+                    section['ABI']           = m.group(2)
+                    section['upload-number'] = m.group(3)
+                else:
+                    debug('The version (%s) failed to match the regular expression.\n' % version, cls.debug)
+
+                retval.append(section)
+                content = []
+                bugs = []
+            else:
+                # find bug numbers and append them to the list
+                for bug_line_match in finditer(cls.bug_rc, line):
+                    bug_matches = findall(cls.bug_nr_rc, bug_line_match.group(0))
+                    bugs.extend(bug_matches)
+
+                content.append(line)
+
+        return retval
+
+    # abi
+    #
+    @classmethod
+    def abi(cls):
+
+        # Check each possible directory for an abi file
+        debian_dirs = cls.debian_directories()
+        retvals = []
+        for debdir in debian_dirs:
+            abidir = debdir + '/abi'
+            debug("Trying '%s': \n" % abidir, cls.debug)
+            if path.isdir(abidir):
+                debug("  '%s' is a directory\n" % abidir, cls.debug)
+                contents = listdir(abidir)
+                for item in contents:
+                    debug("  Contains: '%s'\n" % item, cls.debug)
+                    if path.isdir(abidir + '/' + item):
+                        retvals.append(item)
+                return retvals
+
+        # Not there anywhere, barf
+        raise DebianError('Failed to find the abi files.')
+
+    # abi_arch
+    #
+    @classmethod
+    def abi_arch(cls):
+
+        # Return contents of all arch directories within an ABI
+        debian_dirs = cls.debian_directories()
+        retvals = {}
+        empty = []
+        abiarch = []
+        for debdir in debian_dirs:
+            abidir = debdir + '/abi'
+            debug("Trying '%s': \n" % abidir, cls.debug)
+            if path.isdir(abidir):
+                debug("  '%s' is a directory\n" % abidir, cls.debug)
+                contents = listdir(abidir)
+                for item in contents:
+                    debug("  Contains: '%s'\n" % item, cls.debug)
+                    if path.isdir(abidir + '/' + item):
+                        abisubdirs = listdir(abidir + '/' + item)
+                        for subdir in abisubdirs:
+                            fullpath = path.join(abidir, item, subdir)
+                            if path.isdir(fullpath):
+                                abiarch.append(fullpath)
+                for archdir in abiarch:
+                    arch = path.basename(archdir)
+                    contents = listdir(archdir)
+                    retvals[arch] = contents
+                    # Now, check each ignore file to make sure it's not empty,
+                    # as empty files don't work to trigger ABI ignore in the PPA
+                    # but will in a test build !!!!!
+                    for fname in contents:
+                        if 'ignore' in fname:
+                            fpath = path.join(archdir, fname)
+                            fsize = path.getsize(fpath)
+                            if fsize == 0:
+                                debug("  Found empty ABI file: '%s'\n" % fpath, cls.debug)
+                                empty.append(fpath)
+                return retvals, empty
+
+        # Not there anywhere, barf
+        raise DebianError('Failed to find the abi files.')
+
+
+# vi:set ts=4 sw=4 expandtab:
---
+++ ktl3/git.py
@@ -0,0 +1,244 @@
+#!/usr/bin/env python
+#
+
+from .shell                     import sh
+from re                         import compile
+
+class GitError(Exception):
+    # __init__
+    #
+    def __init__(self, error):
+        self.msg = error
+
+class Git:
+    commit_rc  = compile('^commit\s+([a-f0-9]+)\s*$')
+    author_rc  = compile('^Author:\s+(.*)\s+<(.*)>$')
+    date_rc    = compile('^Date:\s+(.*)$')
+    buglink_rc = compile('^\s+BugLink:\s+http.*launchpad\.net/.*/([0-9]+)$')
+    sob_rc     = compile('^\s+Signed-off-by:\s+(.*)\s+<(.*)>$')
+    ack_rc     = compile('^\s+Acked-by:\s+(.*)\s+<(.*)>$')
+    log_results = {}
+
+    @classmethod
+    def sh(cls, cmd):
+        status, tmp = sh(cmd, quiet=True)
+        output = [l.replace('\n', '') for l in tmp]
+        return status, output
+
+    # is_repo
+    #
+    # If a "git branch" returns a 0 status and a non-empty result, then we
+    # are in a git repository.
+    #
+    @classmethod
+    def is_repo(cls):
+        retval = False
+        try:
+            branches = cls.branches()
+            if branches != "":
+                retval = True
+
+        except GitError:
+            retval = False
+
+        return retval
+
+    # config
+    #
+    # Return the requested git config value. E.g. config("user.email")
+    #
+    @classmethod
+    def config(cls, key):
+        status, result = cls.sh("git config %s" % key)
+        if status != 0:
+            raise GitError(result)
+        return result[0]
+
+    # branches
+    #
+    # Return a list of all the git branches known to this git repository.
+    #
+    @classmethod
+    def branches(cls):
+        retval = []
+        status, result = cls.sh("git branch")
+        if status == 0:
+            for line in result:
+                if line[0] == '*':
+                    line = line[1:]
+                retval.append(line.strip())
+        else:
+            raise GitError(result)
+
+        return retval
+
+    # tags
+    #
+    @classmethod
+    def tags(cls, contains=''):
+        retval = []
+        cmd = "git tag"
+        if contains != '':
+            cmd += ' --contains %s' % (contains)
+
+        status, result = cls.sh(cmd)
+        if status == 0:
+            for line in result:
+                if line != '' and line[0] == '*':
+                    line = line[1:]
+                retval.append(line.strip())
+        else:
+            raise GitError(result)
+
+        return retval
+
+    # current_branch
+    #
+    # Return a string that is the current branch checked out.
+    #
+    @classmethod
+    def current_branch(cls):
+        # Note: older versions of git symbolic-ref do not support --short
+        status, result = cls.sh("git symbolic-ref HEAD")
+        if status != 0:
+            raise GitError("no current branch")
+        return result[0].replace("refs/heads/", "")
+
+    # show
+    #
+    @classmethod
+    def show(cls, obj, branch=''):
+        cmd = 'git show '
+        if branch != '':
+            cmd += branch + ':'
+        cmd += obj
+
+        status, result = cls.sh(cmd)
+        if status != 0:
+            raise GitError(result)
+
+        return result
+
+    @classmethod
+    def __process_log_commit(cls, commit_text, sha1):
+        results = {}
+        results['sha1'] = sha1
+        results['text'] = []
+        for text in commit_text:
+
+            while True:
+                m = cls.author_rc.match(text)          # Author
+                if m is not None:
+                    id = {}
+                    id['name'] = m.group(1)
+                    id['email'] = m.group(2)
+                    results['author'] = id
+                    break
+
+                m = cls.date_rc.match(text)            # Date
+                if m is not None:
+                    results['date'] = m.group(1)
+                    break
+
+                m = cls.buglink_rc.match(text)         # BugLink
+                if m is not None:
+                    bug = m.group(1)
+                    if bug not in cls.log_results['buglink-index']:
+                        cls.log_results['buglink-index'][bug] = []
+
+                    if 'buglink' not in results:
+                        results['buglink'] = []
+
+                    cls.log_results['buglink-index'][bug].append(results['sha1'])
+                    results['buglink'].append(bug)
+                    results['text'].append(text)
+                    break
+
+                m = cls.sob_rc.match(text)             # Signed-off-by
+                if m is not None:
+                    if 'sob' not in results:
+                        results['sob'] = []
+
+                    id = {}
+                    id['name'] = m.group(1)
+                    id['email'] = m.group(2)
+                    results['sob'].append(id)
+                    results['text'].append(text)
+                    break
+
+                m = cls.ack_rc.match(text)             # Acked-by
+                if m is not None:
+                    if 'acks' not in results:
+                        results['acks'] = []
+
+                    id = {}
+                    id['name'] = m.group(1)
+                    id['email'] = m.group(2)
+                    results['acks'].append(id)
+                    results['text'].append(text)
+                    break
+
+                results['text'].append(text)
+                break
+        return results
+
+    # log
+    #
+    # The idea here is to take the output from "git log" and turn it into
+    # a python dictionary structure which can be used by other scripts
+    # to easily get information. One of the most uesful items in this
+    # dictionary is a list of commits indexed by the related buglink.
+    #
+    @classmethod
+    def log(cls, num=-1):
+        cls.log_results = {}
+        cls.log_results['commits']       = []
+        cls.log_results['buglink-index'] = {}
+        if num != -1:
+            status, result = cls.sh("git log -%d" % (num))
+        else:
+            status, result = cls.sh("git log")
+        commit       = {}
+        commit_text  = []
+        current_sha1 = 'unknown'
+        if status != 0:
+            raise GitError(result)
+
+        for line in result:
+            m = cls.commit_rc.match(line)
+            if m is not None:
+                sha1 = m.group(1)
+                # This is a new commit sha1. We are going to build up a dictionary entry
+                # for this one commit and then add it to the dictionary of all commits.
+                #
+                # When building up this commit's dictionary entry, we go back through the
+                # commit text looking for information we want to pull out and make easily
+                # available via dictionary keys.
+                #
+                if len(commit_text) > 0:
+                    commit = cls.__process_log_commit(commit_text, current_sha1)
+                    cls.log_results['commits'].append(commit)
+
+                    # Reset working variables
+                    #
+                    commit = {}
+                    commit_text = []
+                    current_sha1 = sha1
+
+                else:
+                    # This is the very first sha1
+                    #
+                    current_sha1 = m.group(1)
+            else:
+                # This is text between two SHA1s, just add it to the working
+                # buffer for the current commit.
+                #
+                commit_text.append(line)
+
+        if len(commit_text) > 0:
+            commit = cls.__process_log_commit(commit_text, current_sha1)
+            cls.log_results['commits'].append(commit)
+
+        return cls.log_results
+
+# vi:set ts=4 sw=4 expandtab:
