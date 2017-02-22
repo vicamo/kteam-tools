9092f940e23c4dd60ea98b5f11c36832074cd65e
Brad Figg <brad.figg@canonical.com>
2017-02-21 16:28:10 -0800

    promote_to_release.py: Implement the task handler when for the promote-to-release task when it is set to either 'Confirmed' or 'Fix Committed'
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

--- stable/wfl/wft/promote_to_release.py
+++ stable/wfl/wft/promote_to_release.py
@@ -13,8 +13,10 @@ class PromoteToRelease(Promoter):
         center(s.__class__.__name__ + '.__init__')
         super(PromoteToRelease, s).__init__(lp, task, bug)
 
-        s.jumper['New']          = s._ready_for_release
-        s.jumper['Fix Released'] = s._fix_released
+        s.jumper['New']           = s._ready_for_release
+        s.jumper['Confirmed']     = s._verify_promotion
+        s.jumper['Fix Committed'] = s._verify_promotion
+        s.jumper['Fix Released']  = s._fix_released
 
         cleave(s.__class__.__name__ + '.__init__')
 
@@ -24,7 +26,7 @@ class PromoteToRelease(Promoter):
         """
         """
         center(s.__class__.__name__ + '._ready_for_release')
-        retval = False # For the stub
+        retval = False
 
         while True:
 
@@ -41,15 +43,41 @@ class PromoteToRelease(Promoter):
         cleave(s.__class__.__name__ + '._ready_for_release (%s)' % retval)
         return retval
 
+    # _verify_promotion
+    #
+    def _verify_promotion(s):
+        """
+        """
+        center(s.__class__.__name__ + '._verify_promotion')
+        retval = False
+
+        while True:
+
+            # Check if packages were copied to the right pocket->component
+            #
+            if not s.bug.packages_released:
+                cinfo('            packages have not been released', 'yellow')
+                break
+
+            cinfo('    All components are now in -release', 'magenta')
+            s.task.status = 'Fix Released'
+            s.task.timestamp('finished')
+            s.bug.phase = 'Promoted to release'
+            retval = True
+            break
+
+        cleave(s.__class__.__name__ + '._verify_promotion (%s)' % retval)
+        return retval
+
     # _fix_released
     #
     def _fix_released(s):
         """
         """
         center(s.__class__.__name__ + '._fix_released')
-        retval = False # For the stub
+        retval = False
 
-        if s.bug.packages_released and s.task.status != 'Fix Released':
+        if s.bug.packages_released and s.bug.tasks_by_name[s.bug.workflow_project].status != 'Fix Released':
             s.bug.tasks_by_name[s.bug.workflow_project].status = 'Fix Released'
             s.bug.phase = 'Released'
             msgbody = 'The package has been published and the bug is being set to Fix Released\n'
