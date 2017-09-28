98264f50867fbb8ade58c1553ed6726b474d03c6
Kleber Sacilotto de Souza <kleber.souza@canonical.com>
2017-09-28 14:54:30 -0400

    ktl/tracking_bug.py: update snap task name on TB creation
    The snap-relates series (tasks) have been renamed on Launchpad. Update
    the series names while checking if they should be added to the TB, and
    ignore for now the new series.
    
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- ktl/tracking_bug.py
+++ ktl/tracking_bug.py
@@ -64,17 +64,26 @@ class TrackingBug:
             if lp_series.name == 'prepare-package-signed' and not s.has_dependent_package(targeted_series_name, package, 'signed'):
                 cdebug('    no prepare-package-signed', 'yellow')
                 break
-            if lp_series.name == 'kernel-snap-edge' and not s.has_dependent_package(targeted_series_name, package, 'snap-edge'):
-                cdebug('    no kernel-snap-edge', 'yellow')
+            if lp_series.name == 'snap-release-to-edge' and not s.has_dependent_package(targeted_series_name, package, 'snap-edge'):
+                cdebug('    no snap-release-to-edge', 'yellow')
                 break
-            if lp_series.name == 'kernel-snap-beta' and not s.has_dependent_package(targeted_series_name, package, 'snap-beta'):
-                cdebug('    no kernel-snap-beta', 'yellow')
+            if lp_series.name == 'snap-release-to-beta' and not s.has_dependent_package(targeted_series_name, package, 'snap-beta'):
+                cdebug('    no snap-release-to-beta', 'yellow')
                 break
-            if lp_series.name == 'kernel-snap-candidate' and not s.has_dependent_package(targeted_series_name, package, 'snap-candidate'):
-                cdebug('    no kernel-snap-candidate', 'yellow')
+            if lp_series.name == 'snap-release-to-candidate' and not s.has_dependent_package(targeted_series_name, package, 'snap-candidate'):
+                cdebug('    no snap-release-to-candidate', 'yellow')
                 break
-            if lp_series.name == 'kernel-snap-stable' and not s.has_dependent_package(targeted_series_name, package, 'snap-stable'):
-                cdebug('    no kernel-snap-stable', 'yellow')
+            if lp_series.name == 'snap-release-to-stable' and not s.has_dependent_package(targeted_series_name, package, 'snap-stable'):
+                cdebug('    no snap-release-to-stable', 'yellow')
+                break
+            if lp_series.name == 'snap-qa-testing':
+                cdebug('    no snap-qa-testing', 'yellow')
+                break
+            if lp_series.name == 'snap-publish':
+                cdebug('    no snap-publish', 'yellow')
+                break
+            if lp_series.name == 'snap-certification-testing':
+                cdebug('    no snap-certification-testing', 'yellow')
                 break
             retval = True
             break
