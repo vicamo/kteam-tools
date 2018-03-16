521c7e23caf49aa9cebd05ee65b42b6cf83dc634
Andy Whitcroft <apw@canonical.com>
2018-03-16 16:15:14 +0000

    mainline-builds: add preliminary support for multiple priorities
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- ktl/msgq.py
+++ ktl/msgq.py
@@ -16,24 +16,24 @@ class MsgQueue():
         s.channel.exchange_declare(exchange=s.exchange_name, type=exchange_type)
 
 
-    def listen(s, queue_name, routing_key, handler_function, queue_durable=True):
+    def listen(s, queue_name, routing_key, handler_function, queue_durable=True, queue_arguments=None):
         def wrapped_handler(channel, method, properties, body):
             payload = json.loads(body)
             handler_function(payload)
 
-        s.channel.queue_declare(queue_name, durable=queue_durable)
+        s.channel.queue_declare(queue_name, durable=queue_durable, arguments=queue_arguments)
         s.channel.queue_bind(exchange=s.exchange_name, queue=queue_name, routing_key=routing_key)
         s.channel.basic_consume(wrapped_handler, queue=queue_name, no_ack=True)
         s.channel.start_consuming()
 
 
-    def listen_worker(s, queue_name, routing_key, handler_function, queue_durable=True, auto_delete=False):
+    def listen_worker(s, queue_name, routing_key, handler_function, queue_durable=True, auto_delete=False, queue_arguments=None):
         def wrapped_handler(channel, method, properties, body):
             payload = json.loads(body)
             handler_function(payload)
             channel.basic_ack(method.delivery_tag)
 
-        s.channel.queue_declare(queue_name, durable=queue_durable, auto_delete=auto_delete)
+        s.channel.queue_declare(queue_name, durable=queue_durable, auto_delete=auto_delete, arguments=queue_arguments)
         s.channel.queue_bind(exchange=s.exchange_name, queue=queue_name, routing_key=routing_key)
         s.channel.basic_qos(prefetch_count=1)
         s.channel.basic_consume(wrapped_handler, queue=queue_name, no_ack=False)
@@ -64,9 +64,9 @@ class MsgQueue():
         s.channel.queue_delete(queue_name)
 
 
-    def publish(s, routing_key, payload):
+    def publish(s, routing_key, payload, priority=None):
         message_body = json.dumps(payload)
-        properties = pika.BasicProperties(delivery_mode=2)
+        properties = pika.BasicProperties(delivery_mode=2, priority=priority)
         s.channel.basic_publish(exchange=s.exchange_name, routing_key=routing_key, body=message_body, properties=properties)
 
 # vi:set ts=4 sw=4 expandtab:
