7a8ce03706d2f3cb24ccb6cbaa76ada7870fb10f
Andy Whitcroft <apw@canonical.com>
2018-09-18 14:31:07 +0100

    cranky: crl/handle -- add handles
    Adds HandleSet and HandleTree for converting handles to KernelSeries
    Source/Package objects.
    
    Acked-by: Seth Forshee <seth.forshee@canonical.com>
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

---
+++ cranky/crl/handle.py
@@ -0,0 +1,230 @@
+#!/usr/bin/python3
+
+import contextlib
+import os
+import sys
+import yaml
+
+sys.path.append(os.path.realpath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'libs')))
+
+from ktl.kernel_series              import KernelSeries
+from ktl.debian                     import Debian
+
+from crl.config                     import Config
+
+
+@contextlib.contextmanager
+def change_directory(new_dir):
+    previous_dir = os.getcwd()
+    os.chdir(new_dir)
+    try:
+        yield
+    finally:
+        os.chdir(previous_dir)
+
+
+class HandleCore:
+    def encode_directory(self, package):
+        base_path = self.config.lookup(['package-path', 'base-path'], '')
+        for key in (
+            "{series}--{source}--{package}",
+            "{series}--{source}",
+            "{series}",
+            "default",
+            ):
+            key = key.format(
+                series=package.series.codename,
+                source=package.source.name,
+                package=package.name,
+                )
+
+            package_path = self.config.lookup(['package-path', key])
+            if package_path:
+                break
+        if package_path is None:
+            package_path = '{series}/{package}'
+
+        which = package.type if package.type else 'main'
+        which_suffix = '-' + package.type if package.type else ''
+
+        package_path = package_path.format(
+            series=package.series.codename,
+            source=package.source.name,
+            package=package.name,
+            type=which,
+            type_suffix=which_suffix,
+            )
+
+        return os.path.expanduser(os.path.join(base_path, package_path))
+
+    def set_minimal_suffixes(self, source):
+        # Built a set of directory names for all of the packages in the source.
+        paths = {}
+        for package_entry in source.packages:
+            path = self.encode_directory(package_entry)
+            paths[package_entry.type if package_entry.type else 'main'] = path
+
+        # Remove the longest common prefix.
+        prefix = os.path.commonprefix(list(paths.values()))
+        prefix_len = len(prefix)
+        for key in paths:
+            paths[key] = paths[key][prefix_len:]
+
+        # Build a list, longest suffix first.
+        longest = sorted(paths.values(), key=lambda p : -len(p))
+
+        return (longest, paths)
+
+    def identify_directory(self, directory):
+        with change_directory(directory):
+            changelog = Debian.changelog()
+
+        if len(changelog) > 0 and changelog[0]['series'] == 'UNRELEASED':
+            changelog.pop(0)
+
+        if len(changelog) == 0:
+            raise ValueError("Unable to identify directory handle")
+
+        return (changelog[0]['series'], changelog[0]['package'])
+
+
+class HandleTree(HandleCore):
+    def __init__(self, series, package, directory=None, config=None, ks=None):
+        if ks is None:
+            ks = KernelSeries()
+        self.ks = ks
+
+        if config is None:
+            config = Config()
+        self.config = config
+
+        if directory is None:
+            directory = self.encode_directory(package)
+
+        if os.path.exists(directory):
+            (series_name, package_name) = self.identify_directory(directory)
+            if (series_name != series.codename or package_name != package.name):
+                raise ValueError("{}: tree inconsistent, is for {}:{}".format(
+                    directory, series_name, package_name))
+
+        self.series = series
+        self.package = package
+        self.directory = directory
+
+
+class HandleSet(HandleCore):
+    def __init__(self, series, source, trees=None, sample=None, ks=None, config=None):
+        if ks is None:
+            ks = KernelSeries()
+        self.ks = ks
+
+        if config is None:
+            config = Config()
+        self.config = config
+
+        self.series = series
+        self.source = source
+
+        if trees:
+            self.trees = trees
+
+        elif sample is not None:
+            self.trees = [ sample ]
+            (remove, add) = self.set_minimal_suffixes(source)
+
+            prefix = None
+            directory = sample.directory
+            for remove_entry in remove:
+                if remove_entry == '':
+                        prefix = directory
+                        break
+                elif directory.endswith(remove_entry):
+                    prefix = directory[:-len(remove_entry)]
+                    break
+
+            if prefix is None:
+                raise ValueError("{}: directory handle does not match set forms", handle)
+
+            for package_entry in source.packages:
+                if package_entry == sample.package:
+                    continue
+                directory = prefix + add[package_entry.type if package_entry.type else 'main']
+                self.trees.append(HandleTree(series, package_entry, directory,
+                     ks=self.ks, config=self.config))
+
+        else:
+            self.trees = []
+            for package_entry in source.packages:
+                self.trees.append(HandleTree(series, package_entry, ks=self.ks, config=self.config))
+
+
+class Handle(HandleCore):
+    def __init__(self, config=None, ks=None):
+        if ks is None:
+            ks = KernelSeries()
+        self.ks = ks
+
+        if config is None:
+            config = Config()
+        self.config = config
+
+    def lookup_tree(self, handle):
+        series = None
+        package = None
+        directory = None
+
+        # A directory passed as a handle.
+        if os.path.exists(handle):
+            (series_name, package_name) = self.identify_directory(handle)
+            directory = handle
+
+        else:
+            bits = handle.split(':')
+            if len(bits) != 2:
+                raise ValueError("{}: handle format unknown".format(handle))
+
+            (series_name, package_name) = bits
+
+        series = self.ks.lookup_series(codename=series_name)
+        if series is None:
+            raise ValueError("{}: handle directory contains unknown series".format(handle))
+
+        for source_entry in series.sources:
+            for package_entry in source_entry.packages:
+                if package_entry.name == package_name:
+                    package = package_entry
+                    break
+            if package:
+                break
+
+        if package is None:
+            raise ValueError("{}: handle directory contains unknown package".format(handle))
+
+        return HandleTree(series, package, directory, ks=self.ks, config=self.config)
+
+    def lookup_set(self, handle):
+        series = None
+        source = None
+
+        # A directory passed as a handle.
+        if os.path.exists(handle):
+            tree = self.lookup_tree(handle)
+            return HandleSet(tree.series, tree.package.source, sample=tree, ks=self.ks, config=self.config)
+
+        # Validate this as a series/package handle.
+        else:
+            bits = handle.split(':')
+            if len(bits) != 2:
+                raise ValueError("{}: handle format unknown".format(handle))
+
+            (series_name, source_name) = bits
+
+            series = self.ks.lookup_series(codename=series_name)
+            if series is None:
+                raise ValueError("{}: handle contains unknown series".format(series_name))
+
+            source = series.lookup_source(source_name)
+            if source is None:
+                raise ValueError("{}: handle contains unknown source".format(source_name))
+
+            return HandleSet(series, source, ks=self.ks, config=self.config)
---
+++ cranky/crl/test_handle.py
@@ -0,0 +1,493 @@
+#!/usr/bin/python3
+
+import os
+import sys
+from testfixtures               import TempDirectory
+import unittest
+from subprocess                 import getstatusoutput
+
+sys.path.append(os.path.realpath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'libs')))
+sys.path.append(os.pardir)
+
+from handle                     import Handle, change_directory
+from ktl.kernel_series          import KernelSeries
+from config                     import Config
+
+class TestHandle(unittest.TestCase):
+
+    data_yaml = """
+    '18.04':
+        codename: bionic
+        sources:
+            linux:
+                packages:
+                    linux:
+                    linux-meta:
+                        type: meta
+            linux2:
+                packages:
+                    linux2-main:
+                    linux2-meta:
+                        type: meta
+    '17.10':
+        codename: artful
+        sources:
+            linux:
+                packages:
+                    linux:
+                    linux-meta:
+                        type: meta
+    '16.04':
+        codename: xenial
+        sources:
+            linux:
+                packages:
+                    linux:
+                    linux-meta:
+                        type: meta
+    """
+
+    path_config_yaml = """
+    package-path:
+        artful--linux--linux-meta:  artful/meta
+        artful--linux:              'artful/{package}'
+        xenial:                     '{series}-old/{package}'
+        default:                    '{series}/{package}'
+    """
+    basepath_config_yaml = path_config_yaml + """
+        base-path:                  '/src'
+    """
+
+    def assertEqualTrees(self, expected, trees_raw):
+        trees = []
+        for tree_entry in trees_raw:
+            trees.append([tree_entry.series.codename,
+                tree_entry.package.name, tree_entry.directory])
+
+        self.assertEqual(sorted(expected), sorted(trees))
+
+
+class TestHandleSeries(TestHandle):
+
+    def test_series_source(self):
+        ks = KernelSeries(data=self.data_yaml)
+        hdl = Handle(ks=ks).lookup_set('bionic:linux')
+
+        self.assertEqual(hdl.source.series.codename, 'bionic')
+        self.assertEqual(hdl.source.name, 'linux')
+
+    def test_series_source_main_missmatch(self):
+        ks = KernelSeries(data=self.data_yaml)
+        hdl = Handle(ks=ks).lookup_set('bionic:linux2')
+
+        self.assertEqual(hdl.source.series.codename, 'bionic')
+        self.assertEqual(hdl.source.name, 'linux2')
+
+    def test_series_package(self):
+        ks = KernelSeries(data=self.data_yaml)
+        hdl = Handle(ks=ks).lookup_tree('bionic:linux-meta')
+
+        self.assertEqual(hdl.package.series.codename, 'bionic')
+        self.assertEqual(hdl.package.name, 'linux-meta')
+
+    def test_series_package_main_missmatch(self):
+        ks = KernelSeries(data=self.data_yaml)
+        hdl = Handle(ks=ks).lookup_tree('bionic:linux2-main')
+
+        self.assertEqual(hdl.package.series.codename, 'bionic')
+        self.assertEqual(hdl.package.name, 'linux2-main')
+
+    def test_trees_series_bionic(self):
+        ks = KernelSeries(data=self.data_yaml)
+        config = Config(data=self.path_config_yaml)
+        hdl = Handle(ks=ks, config=config).lookup_set('bionic:linux')
+
+        trees_match = [
+            [ 'bionic', 'linux', 'bionic/linux' ],
+            [ 'bionic', 'linux-meta', 'bionic/linux-meta' ],
+            ]
+        self.assertEqualTrees(trees_match, hdl.trees)
+
+    def test_trees_series_artful(self):
+        ks = KernelSeries(data=self.data_yaml)
+        config = Config(data=self.path_config_yaml)
+        hdl = Handle(ks=ks, config=config).lookup_set('artful:linux')
+
+        trees_match = [
+            [ 'artful', 'linux', 'artful/linux' ],
+            [ 'artful', 'linux-meta', 'artful/meta' ],
+            ]
+        self.assertEqualTrees(trees_match, hdl.trees)
+
+    def test_trees_series_bionic_basepath(self):
+        ks = KernelSeries(data=self.data_yaml)
+        config = Config(data=self.basepath_config_yaml)
+        hdl = Handle(ks=ks, config=config).lookup_set('bionic:linux')
+
+        trees_match = [
+            [ 'bionic', 'linux', '/src/bionic/linux' ],
+            [ 'bionic', 'linux-meta', '/src/bionic/linux-meta' ],
+            ]
+        self.assertEqualTrees(trees_match, hdl.trees)
+
+
+class TestHandleDirectoryEncode(TestHandle):
+
+    def test_encode_directory_series(self):
+        config_yaml = """
+        package-path:
+            default:    '{series}'
+        """
+        ks = KernelSeries(data=self.data_yaml)
+        config = Config(data=config_yaml)
+        hdl = Handle(ks=ks, config=config).lookup_tree('bionic:linux-meta')
+
+        directory = hdl.encode_directory(hdl.package)
+
+        self.assertEqual(directory, 'bionic')
+
+    def test_encode_directory_source(self):
+        config_yaml = """
+        package-path:
+            default:    '{source}'
+        """
+        ks = KernelSeries(data=self.data_yaml)
+        config = Config(data=config_yaml)
+        hdl = Handle(ks=ks, config=config).lookup_tree('bionic:linux-meta')
+
+        directory = hdl.encode_directory(hdl.package)
+
+        self.assertEqual(directory, 'linux')
+
+    def test_encode_directory_package(self):
+        config_yaml = """
+        package-path:
+            default:    '{package}'
+        """
+        ks = KernelSeries(data=self.data_yaml)
+        config = Config(data=config_yaml)
+        hdl = Handle(ks=ks, config=config).lookup_tree('bionic:linux-meta')
+
+        directory = hdl.encode_directory(hdl.package)
+
+        self.assertEqual(directory, 'linux-meta')
+
+    def test_encode_directory_type_main(self):
+        config_yaml = """
+        package-path:
+            default:    '{type}'
+        """
+        ks = KernelSeries(data=self.data_yaml)
+        config = Config(data=config_yaml)
+        hdl = Handle(ks=ks, config=config).lookup_tree('bionic:linux')
+
+        directory = hdl.encode_directory(hdl.package)
+
+        self.assertEqual(directory, 'main')
+
+    def test_encode_directory_type_meta(self):
+        config_yaml = """
+        package-path:
+            default:    '{type}'
+        """
+        ks = KernelSeries(data=self.data_yaml)
+        config = Config(data=config_yaml)
+        hdl = Handle(ks=ks, config=config).lookup_tree('bionic:linux-meta')
+
+        directory = hdl.encode_directory(hdl.package)
+
+        self.assertEqual(directory, 'meta')
+
+    def test_encode_directory_typesuffix_main(self):
+        config_yaml = """
+        package-path:
+            default:    '{type_suffix}'
+        """
+        ks = KernelSeries(data=self.data_yaml)
+        config = Config(data=config_yaml)
+        hdl = Handle(ks=ks, config=config).lookup_tree('bionic:linux')
+
+        directory = hdl.encode_directory(hdl.package)
+
+        self.assertEqual(directory, '')
+
+    def test_encode_directory_typesuffix_meta(self):
+        config_yaml = """
+        package-path:
+            default:    '{type_suffix}'
+        """
+        ks = KernelSeries(data=self.data_yaml)
+        config = Config(data=config_yaml)
+        hdl = Handle(ks=ks, config=config).lookup_tree('bionic:linux-meta')
+
+        directory = hdl.encode_directory(hdl.package)
+
+        self.assertEqual(directory, '-meta')
+
+    def test_encode_directory_bionic_linux(self):
+        ks = KernelSeries(data=self.data_yaml)
+        config = Config(data=self.path_config_yaml)
+        hdl = Handle(ks=ks, config=config).lookup_tree('bionic:linux')
+
+        directory = hdl.encode_directory(hdl.package)
+
+        self.assertEqual(directory, 'bionic/linux')
+        self.assertEqual(hdl.directory, 'bionic/linux')
+
+    def test_encode_directory_bionic_linux_meta(self):
+        ks = KernelSeries(data=self.data_yaml)
+        config = Config(data=self.path_config_yaml)
+        hdl = Handle(ks=ks, config=config).lookup_tree('bionic:linux-meta')
+
+        directory = hdl.encode_directory(hdl.package)
+
+        self.assertEqual(directory, 'bionic/linux-meta')
+        self.assertEqual(hdl.directory, 'bionic/linux-meta')
+
+    def test_encode_directory_artful_linux(self):
+        ks = KernelSeries(data=self.data_yaml)
+        config = Config(data=self.path_config_yaml)
+        hdl = Handle(ks=ks, config=config).lookup_tree('artful:linux')
+
+        directory = hdl.encode_directory(hdl.package)
+
+        self.assertEqual(directory, 'artful/linux')
+
+    def test_encode_directory_artful_linux_meta(self):
+        ks = KernelSeries(data=self.data_yaml)
+        config = Config(data=self.path_config_yaml)
+        hdl = Handle(ks=ks, config=config).lookup_tree('artful:linux-meta')
+
+        directory = hdl.encode_directory(hdl.package)
+
+        self.assertEqual(directory, 'artful/meta')
+
+    def test_encode_directory_xenial_linux(self):
+        ks = KernelSeries(data=self.data_yaml)
+        config = Config(data=self.path_config_yaml)
+        hdl = Handle(ks=ks, config=config).lookup_tree('xenial:linux')
+
+        directory = hdl.encode_directory(hdl.package)
+
+        self.assertEqual(directory, 'xenial-old/linux')
+
+    def test_encode_directory_xenial_linux_meta(self):
+        ks = KernelSeries(data=self.data_yaml)
+        config = Config(data=self.path_config_yaml)
+        hdl = Handle(ks=ks, config=config).lookup_tree('xenial:linux-meta')
+
+        directory = hdl.encode_directory(hdl.package)
+
+        self.assertEqual(directory, 'xenial-old/linux-meta')
+
+
+class TestHandleDirectory(TestHandle):
+
+    def setUpRunCmd(self, path, cmd):
+        with change_directory(path):
+            (status, output) = getstatusoutput(cmd)
+            if status != 0:
+                print("CMD FAILED:", output)
+            self.assertEqual(status, 0)
+
+    def setUpSourceMain(self, d, name, branch, series, package):
+        git_path = d.path + '/' + name
+
+        d.makedir(name)
+        self.setUpRunCmd(git_path, 'git init')
+
+        debian = 'debian.' + branch
+
+        data_env = "DEBIAN={}\n".format(debian)
+        d.makedir(name + '/debian')
+        d.write(name + '/debian/debian.env', data_env.encode('utf-8'))
+        self.setUpRunCmd(git_path, 'git add debian/debian.env')
+
+        data_changelog = "{} ({}) {}; urgency=medium\n  * foo\n -- ME\n".format(package, '4.15.0-1.2', series)
+        d.makedir(name + '/' + debian)
+        d.write(name + '/' + debian + '/changelog', data_changelog.encode('utf-8'))
+        self.setUpRunCmd(git_path, 'git add {}/changelog'.format(debian))
+
+        self.setUpRunCmd(git_path, 'git commit -a -m "Initial"'.format(debian))
+
+    def setUpSourceMeta(self, d, name, branch, series, package):
+        git_path = d.path + '/' + name
+
+        d.makedir(name)
+        self.setUpRunCmd(git_path, 'git init')
+
+        data_changelog = "{} ({}) {}; urgency=medium\n  * foo\n -- ME\n".format(package, '4.15.0-1.2', series)
+        d.makedir(name + '/debian')
+        d.write(name + '/debian/changelog', data_changelog.encode('utf-8'))
+        self.setUpRunCmd(git_path, 'git add debian/changelog'.format(debian))
+
+        self.setUpRunCmd(git_path, 'git commit -a -m "Initial"'.format(debian))
+
+    def test_directory_source_noconfig(self):
+        ks = KernelSeries(data=self.data_yaml)
+        with TempDirectory() as d:
+            self.setUpSourceMain(d, 'bionic/linux', 'master', 'bionic', 'linux')
+
+            config = Config(data="")
+
+            hdl = Handle(ks=ks, config=config).lookup_set(d.getpath('bionic/linux'))
+
+            self.assertEqual(hdl.series.codename, 'bionic')
+            self.assertEqual(hdl.source.name, 'linux')
+
+    def test_directory_source_config_bionic_linux(self):
+        ks = KernelSeries(data=self.data_yaml)
+        with TempDirectory() as d:
+            self.setUpSourceMain(d, 'bionic/linux', 'master', 'bionic', 'linux')
+
+            config = Config(data=self.path_config_yaml)
+
+            hdl = Handle(ks=ks, config=config).lookup_set(d.getpath('bionic/linux'))
+
+            self.assertEqual(hdl.series.codename, 'bionic')
+            self.assertEqual(hdl.source.name, 'linux')
+
+    def test_directory_source_config_bionic_meta(self):
+        ks = KernelSeries(data=self.data_yaml)
+        with TempDirectory() as d:
+            self.setUpSourceMain(d, 'bionic/linux-meta', 'master', 'bionic', 'linux-meta')
+
+            config = Config(data=self.path_config_yaml)
+
+            hdl = Handle(ks=ks, config=config).lookup_set(d.getpath('bionic/linux-meta'))
+
+            self.assertEqual(hdl.series.codename, 'bionic')
+            self.assertEqual(hdl.source.name, 'linux')
+
+    def test_directory_tree_config_bionic_linux(self):
+        ks = KernelSeries(data=self.data_yaml)
+        with TempDirectory() as d:
+            self.setUpSourceMain(d, 'bionic/linux', 'master', 'bionic', 'linux')
+
+            config = Config(data=self.path_config_yaml)
+
+            hdl = Handle(ks=ks, config=config).lookup_tree(d.getpath('bionic/linux'))
+
+            self.assertEqual(hdl.series.codename, 'bionic')
+            self.assertEqual(hdl.package.name, 'linux')
+            self.assertEqual(hdl.directory, d.getpath('bionic/linux'))
+
+    def test_directory_tree_config_bionic_meta(self):
+        ks = KernelSeries(data=self.data_yaml)
+        with TempDirectory() as d:
+            self.setUpSourceMain(d, 'bionic/linux-meta', 'master', 'bionic', 'linux-meta')
+
+            config = Config(data=self.path_config_yaml)
+
+            hdl = Handle(ks=ks, config=config).lookup_tree(d.getpath('bionic/linux-meta'))
+
+            self.assertEqual(hdl.series.codename, 'bionic')
+            self.assertEqual(hdl.package.name, 'linux-meta')
+            self.assertEqual(hdl.directory, d.getpath('bionic/linux-meta'))
+
+    def test_trees_directory_source_config_bionic_linux(self):
+        ks = KernelSeries(data=self.data_yaml)
+        with TempDirectory() as d:
+            self.setUpSourceMain(d, 'bionic/linux', 'master', 'bionic', 'linux')
+
+            config = Config(data=self.path_config_yaml)
+
+            hdl = Handle(ks=ks, config=config).lookup_set(d.getpath('bionic/linux'))
+
+            trees_match = [
+                [ 'bionic', 'linux', d.getpath('bionic/linux') ],
+                [ 'bionic', 'linux-meta', d.getpath('bionic/linux-meta') ],
+                ]
+            self.assertEqualTrees(trees_match, hdl.trees)
+
+    def test_trees_directory_source_config_bionic_meta(self):
+        ks = KernelSeries(data=self.data_yaml)
+        with TempDirectory() as d:
+            self.setUpSourceMain(d, 'bionic/linux-meta', 'master', 'bionic', 'linux-meta')
+
+            config = Config(data=self.path_config_yaml)
+
+            hdl = Handle(ks=ks, config=config).lookup_set(d.getpath('bionic/linux-meta'))
+
+            trees_match = [
+                [ 'bionic', 'linux', d.getpath('bionic/linux') ],
+                [ 'bionic', 'linux-meta', d.getpath('bionic/linux-meta') ],
+                ]
+            self.assertEqualTrees(trees_match, hdl.trees)
+
+    def test_trees_directory_source_config_bionic_linux_metapresent(self):
+        ks = KernelSeries(data=self.data_yaml)
+        with TempDirectory() as d:
+            self.setUpSourceMain(d, 'bionic/linux', 'master', 'bionic', 'linux')
+            self.setUpSourceMain(d, 'bionic/linux-meta', 'master', 'bionic', 'linux-meta')
+
+            config = Config(data=self.path_config_yaml)
+
+            hdl = Handle(ks=ks, config=config).lookup_set(d.getpath('bionic/linux'))
+
+            trees_match = [
+                [ 'bionic', 'linux', d.getpath('bionic/linux') ],
+                [ 'bionic', 'linux-meta', d.getpath('bionic/linux-meta') ],
+                ]
+            self.assertEqualTrees(trees_match, hdl.trees)
+
+    def test_trees_directory_source_config_bionic_linux_metabroken(self):
+        ks = KernelSeries(data=self.data_yaml)
+        with TempDirectory() as d:
+            self.setUpSourceMain(d, 'bionic/linux', 'master', 'bionic', 'linux')
+            self.setUpSourceMain(d, 'bionic/linux-meta', 'master', 'bionic', 'linux-meta-hwe')
+
+            config = Config(data=self.path_config_yaml)
+
+            with self.assertRaises(ValueError):
+                hdl = Handle(ks=ks, config=config).lookup_set(d.getpath('bionic/linux'))
+
+
+class TestHandleSuffixes(TestHandle):
+
+    def test_minimal_suffixes_bionic_main(self):
+        ks = KernelSeries(data=self.data_yaml)
+        config = Config(data=self.path_config_yaml)
+
+        hdl = Handle(ks=ks, config=config).lookup_tree('bionic:linux')
+
+        (remove, add) = hdl.set_minimal_suffixes(hdl.package.source)
+
+        remove_match = [ '-meta', '', ]
+        add_match = { 'main': '', 'meta': '-meta', }
+
+        self.assertEqual(remove, remove_match)
+        self.assertEqual(add, add_match)
+
+    def test_minimal_suffixes_artful(self):
+        ks = KernelSeries(data=self.data_yaml)
+        config = Config(data=self.path_config_yaml)
+
+        hdl = Handle(ks=ks, config=config).lookup_tree('artful:linux')
+
+        (remove, add) = hdl.set_minimal_suffixes(hdl.package.source)
+
+        remove_match = [ 'linux', 'meta', ]
+        add_match = { 'main': 'linux', 'meta': 'meta', }
+
+        self.assertEqual(remove, remove_match)
+        self.assertEqual(add, add_match)
+
+    def test_minimal_suffixes_xenial(self):
+        ks = KernelSeries(data=self.data_yaml)
+        config = Config(data=self.path_config_yaml)
+
+        hdl = Handle(ks=ks, config=config).lookup_tree('xenial:linux')
+
+        (remove, add) = hdl.set_minimal_suffixes(hdl.package.source)
+
+        remove_match = [ '-meta', '', ]
+        add_match = { 'main': '', 'meta': '-meta', }
+
+        self.assertEqual(remove, remove_match)
+        self.assertEqual(add, add_match)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
