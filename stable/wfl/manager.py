8257ea309092c04780963c009adec5467bd54ae0
Andy Whitcroft <apw@canonical.com>
2018-10-31 06:50:13 +0000

    swm: manager -- elide duplicate bugs from the list
    We do not want to scan duplicate bugs even if they are marked
    as live.  Drop any which are marked duplicates when scanning
    bug tasks.
    
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- stable/wfl/manager.py
+++ stable/wfl/manager.py
@@ -105,6 +105,9 @@ class WorkflowManager():
 
                 for task in tasks:
                     bug = task.bug
+                    if bug.duplicate_of is not None:
+                        cinfo('    LP: #%s - %s (DUPLICATE)' % (bug.id, bug.title), 'magenta')
+                        continue
                     cinfo('    LP: #%s - %s' % (bug.id, bug.title), 'magenta')
                     retval[str(bug.id)] = bug.title
 
