7867385f0d46a71ceded9b79f8aee69208a06a4d
Andy Whitcroft <apw@canonical.com>
2018-11-22 16:47:26 +0000

    swm: swm-config -- add deployment-blackout data parsing
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- stable/wfl/swm_config.py
+++ stable/wfl/swm_config.py
@@ -1,6 +1,7 @@
 #
 # swm_config -- expose the kernel-series swm_data
 #
+from datetime                   import datetime
 import yaml
 
 from wfl.errors                 import ShankError
@@ -36,8 +37,32 @@ class SwmConfig:
             data = {}
         self._data = data
 
+        # Parse and validate the deployment-blackout.
+        self._blackouts = []
+        blackout_list = self._data.get('deployment-blackout')
+        if blackout_list is None:
+            blackout_list = []
+        for blackout in blackout_list:
+            if len(blackout) != 2:
+                raise SwmConfigError("bad blackout entry -- start and end dates required")
+            try:
+                start = datetime.strptime(blackout[0], '%Y-%m-%d %H:%M')
+            except ValueError:
+                raise SwmConfigError("bad blackout entry -- invalid start date")
+            try:
+                end = datetime.strptime(blackout[1], '%Y-%m-%d %H:%M')
+            except ValueError:
+                raise SwmConfigError("bad blackout entry -- invalid end date")
+            self._blackouts.append([start, end])
+
         cleave(self.__class__.__name__ + '.__init__')
 
     @property
     def gcp_nvidia_packages(self):
         return self._data.get('gcp-nvidia-packages', False)
+
+    def in_blackout(self, when):
+        for blackout in self._blackouts:
+            if when >= blackout[0] and when < blackout[1]:
+                return True
+        return False
--- stable/wfl/test_swm_config.py
+++ stable/wfl/test_swm_config.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+from datetime           import datetime
 import os
 import sys
 from testfixtures       import (
@@ -67,6 +68,89 @@ class TestSwmConfig(TestSwmConfigCore):
 
         self.assertIsNotNone(sc)
         self.assertTrue(sc.gcp_nvidia_packages)
+
+    def test_deployment_blackout_present_empty(self):
+        data = """
+            deployment-blackout:
+        """
+        sc = SwmConfig(data=data)
+
+        self.assertIsNotNone(sc)
+        now = datetime(year=2018, month=1, day=1, hour=10, minute=0)
+        self.assertFalse(sc.in_blackout(now))
+
+    def test_deployment_blackout_present_one(self):
+        data = """
+            deployment-blackout:
+                - ['2018-01-02 08:00', '2018-01-07 08:00']
+        """
+        sc = SwmConfig(data=data)
+        self.assertIsNotNone(sc)
+        now = datetime(year=2018, month=1, day=1, hour=10, minute=0)
+        self.assertFalse(sc.in_blackout(now))
+        now = datetime(year=2018, month=1, day=2, hour=10, minute=0)
+        self.assertTrue(sc.in_blackout(now))
+        now = datetime(year=2018, month=1, day=8, hour=10, minute=0)
+        self.assertFalse(sc.in_blackout(now))
+
+    def test_deployment_blackout_present_one_edges(self):
+        data = """
+            deployment-blackout:
+                - ['2018-01-02 08:00', '2018-01-07 08:00']
+        """
+        sc = SwmConfig(data=data)
+        self.assertIsNotNone(sc)
+        now = datetime(year=2018, month=1, day=2, hour=7, minute=59)
+        self.assertFalse(sc.in_blackout(now))
+        now = datetime(year=2018, month=1, day=2, hour=8, minute=0)
+        self.assertTrue(sc.in_blackout(now))
+        now = datetime(year=2018, month=1, day=7, hour=7, minute=59)
+        self.assertTrue(sc.in_blackout(now))
+        now = datetime(year=2018, month=1, day=7, hour=8, minute=0)
+        self.assertFalse(sc.in_blackout(now))
+
+    def test_deployment_blackout_present_multiple(self):
+        data = """
+            deployment-blackout:
+                - ['2018-01-02 08:00', '2018-01-07 08:00']
+                - ['2018-01-09 08:00', '2018-01-12 08:00']
+        """
+        sc = SwmConfig(data=data)
+        self.assertIsNotNone(sc)
+        now = datetime(year=2018, month=1, day=1, hour=10, minute=0)
+        self.assertFalse(sc.in_blackout(now))
+        now = datetime(year=2018, month=1, day=2, hour=10, minute=0)
+        self.assertTrue(sc.in_blackout(now))
+        now = datetime(year=2018, month=1, day=8, hour=10, minute=0)
+        self.assertFalse(sc.in_blackout(now))
+        now = datetime(year=2018, month=1, day=9, hour=10, minute=0)
+        self.assertTrue(sc.in_blackout(now))
+        now = datetime(year=2018, month=1, day=12, hour=10, minute=0)
+        self.assertFalse(sc.in_blackout(now))
+
+    def test_deployment_blackout_short_entry(self):
+        data = """
+            deployment-blackout:
+                - '2018-01-01 10:00'
+        """
+        with self.assertRaises(SwmConfigError):
+            sc = SwmConfig(data=data)
+
+    def test_deployment_blackout_junk_start_entry(self):
+        data = """
+            deployment-blackout:
+                - ['junk', '2018-01-07 08:00']
+        """
+        with self.assertRaises(SwmConfigError):
+            sc = SwmConfig(data=data)
+
+    def test_deployment_blackout_junk_end_entry(self):
+        data = """
+            deployment-blackout:
+                - ['2018-01-02 08:00', 'junk']
+        """
+        with self.assertRaises(SwmConfigError):
+            sc = SwmConfig(data=data)
 
 if __name__ == '__main__':
     unittest.main()
