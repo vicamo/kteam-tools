89949d469c5549a55198ef4d605aadc13a111fc9
Andy Whitcroft <apw@canonical.com>
2018-12-06 17:16:00 +0100

    cranky-fix: lib.sh -- commit handle addition and removals
    Extend commit to handle addition and removals of files.  This means we
    no longer need to touch/git add new files before calling the commit
    helper.  We also now have a mechanism to remove files.
    
    Acked-by: Thadeu Lima de Souza Cascardo <cascardo@canonical.com>
    Signed-off-by: Andy Whitcroft <apw@canonical.com>
    Acked-by: Marcelo Cerri <marcelo.cerri@canonical.com>
    Acked-by: Juerg Haefliger <juerg.haefliger@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- cranky/fixes.d/lib.sh
+++ cranky/fixes.d/lib.sh
@@ -24,15 +24,29 @@ commit()
 {
 	local msg="$1"
 	shift 1
+	local file
 	msg="$msg
 
 $FIX_BUGLINK
 "
-	if ! git diff --exit-code -- "$@" >/dev/null; then
-		git commit -m "$msg" -s -- "$@"
+	if [ "$#" -eq 0 ]; then
+		fix_verbose "no files present"
+		return
+	fi
+	for file in "$@"
+	do
+		if [ -e "$file" ]; then
+			git add "$file"
+		else
+			git rm --ignore-unmatch "$file"
+		fi
+	done
+	if ! git diff --exit-code HEAD -- "$@" >/dev/null; then
+		git commit -m "$msg" -s --untracked-files=no -- "$@"
 	else
 		fix_verbose "no changes to commit"
 	fi
+
 }
 
 fix_uses_master()
@@ -49,13 +63,17 @@ resync_master()
 	shift 1
 	local master="$FIX_MASTER"
 	local file
+	local files=()
 
 	fix_uses_master
 
 	for file in "$@"
 	do
-		[ -f "$master/$file" ] && [ -f "$file" ] && cp -p "$master/$file" "$file"
+		if [ -f "$master/$file" ] && [ -f "$file" ]; then
+			cp -p "$master/$file" "$file"
+			files+=("$file")
+		fi
 	done
 
-	commit "$msg" "$@"
+	commit "$msg" "${files[@]}"
 }
