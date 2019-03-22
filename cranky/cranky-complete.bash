64a13f53d5aeb1a90694f8a4cf2f7171a31d0965
Marcelo Henrique Cerri <marcelo.cerri@canonical.com>
2019-03-22 14:41:23 -0300

    cranky/cranky-complete.bash: Make cmds a local variable
    To avoid polluting the environment. Also sort the list of commands.
    
    Acked-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>
    Signed-off-by: Marcelo Henrique Cerri <marcelo.cerri@canonical.com>

--- cranky/cranky-complete.bash
+++ cranky/cranky-complete.bash
@@ -4,30 +4,28 @@
 #
 # . /path/to/kteam-tools/cranky/cranky-complete.bash
 #
-
-cmds=(
-	"build-sources -h --help"
-	"chroot -h --help create-base create-session map-session run"
-	"clone -h --help -r --reference -d --dissociate"
-	"checkout -h --help -r --reference -d --dissociate"
-	"close -h --help -d --dry-run -c --include-config -s --skip-master"
-	"fix -h --help"
-	"link-tb -h --help -d --dry-run -r --re-run -s --sru-cycle"
-	"list-routing -h --help -v --verbose"
-	"open -h --help -d --dry-run -r --reuse-abi"
-	"rebase -h -r -b -l -d "
-	"reorder -h --help -d --dry-run"
-	"review -h --help -p --prev-dsc-dir -o --output"
-	"rmadison -h --help -a --show-all -e --show-extended"
-	"tag -h --help -v --verbose -f --force"
-	"test-build -h --help -a --arch -c --commit -d --dry-run -f --fail -p --purge -t --target"
-	"update-snap -h --help --dry-run -u --updates -t --no-tags"
-	"updateconfigs -h --help -c --clean"
-	"fdr -h --help -c --clean"
-	"spin -h --help -d --dry-run --devel"
-)
-
 _cranky() {
+	local cmds=(
+		"build-sources -h --help"
+		"checkout -h --help -r --reference -d --dissociate"
+		"chroot -h --help create-base create-session map-session run"
+		"clone -h --help -r --reference -d --dissociate"
+		"close -h --help -d --dry-run -c --include-config -s --skip-master"
+		"fdr -h --help -c --clean"
+		"fix -h --help"
+		"link-tb -h --help -d --dry-run -r --re-run -s --sru-cycle"
+		"list-routing -h --help -v --verbose"
+		"open -h --help -d --dry-run -r --reuse-abi"
+		"rebase -h -r -b -l -d "
+		"reorder -h --help -d --dry-run"
+		"review -h --help -p --prev-dsc-dir -o --output"
+		"rmadison -h --help -a --show-all -e --show-extended"
+		"spin -h --help -d --dry-run --devel"
+		"tag -h --help -v --verbose -f --force"
+		"test-build -h --help -a --arch -c --commit -d --dry-run -f --fail -p --purge -t --target"
+		"update-snap -h --help --dry-run -u --updates -t --no-tags"
+		"updateconfigs -h --help -c --clean"
+	)
 	local opts=
 	if [ "$COMP_CWORD" -eq 1 ]; then
 		for cmd in "${cmds[@]}"; do
