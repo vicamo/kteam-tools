9f8d3508de39ca585f97b140e32285c075f68194
Andy Whitcroft <apw@canonical.com>
2019-05-02 12:01:04 +0100

    ktl/kernel_series: drop inadvertant application of warnings
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- ktl/kernel_series.py
+++ ktl/kernel_series.py
@@ -7,7 +7,6 @@ except ImportError:
     from urllib2 import urlopen
 
 import os
-from warnings import warn
 import yaml
 
 class KernelRoutingEntry:
@@ -193,7 +192,6 @@ class KernelSnapEntry:
     @property
     def arches(self):
         # XXX: should this be []
-        warn('arches replaced by publish-to', PendingDeprecationWarning)
         return self._data.get('arches', None)
 
     @property
