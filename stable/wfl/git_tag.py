3782a59dc23c065764283eb34f025c4984626976
Andy Whitcroft <apw@canonical.com>
2019-07-10 13:17:58 +0100

    swm: git_tag -- switch to using git ls-remote to handle private repositories
    Currently we can only verify data for public git repositories.  Switch
    to using git ls-remote <respository> and parsing the returned refs.
    By careful configuration of git (see below) we can ensure that this works
    for both public and private repositories.
    
    This requires the calling user to have an ssh key in launchpad and for
    the git prefixes required to be configured to use ssh and the appropriate
    launchpad account in .gitconfig, something like:
    
        [url "git+ssh://ubuntu-kernel-bot@git.launchpad.net/"]
            insteadof = lp:
            insteadof = git://git.launchpad.net/
            insteadof = https://git.launchpad.net/
    
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- stable/wfl/git_tag.py
+++ stable/wfl/git_tag.py
@@ -1,9 +1,4 @@
-try:
-    from urllib.request         import urlopen, Request, ProxyHandler, build_opener, install_opener
-    from urllib.error           import URLError, HTTPError
-    from urllib.parse           import quote_plus
-except ImportError:
-    from urllib2                import urlopen, Request, URLError, HTTPError, ProxyHandler, build_opener, install_opener, quote_plus
+from subprocess                 import Popen, PIPE, run
 
 from wfl.errors                 import ShankError
 from wfl.log                    import center, cleave, cinfo, cdebug
@@ -30,53 +25,61 @@ class GitTag():
         self.package = package
         self.version = version
 
-        self.verifiable = False
+        self._verifiable = False
         self._present = False
 
         if package.repo is not None and package.repo.url is not None:
+            url = package.repo.url
+            self._refs = self.cache_tags(url)
+
             tag = 'Ubuntu{}-{}'.format(
                 package.source.name.replace('linux', ''),
                 version.replace('~', '_'))
-            url = package.repo.url
 
             cdebug('pkg name: {}'.format(package.source.name))
             cdebug('version : {}'.format(version))
 
-            self.lookup_tag(url, tag)
-            if not self.present and '-edge' in tag:
-                tag = tag.replace('-edge', '')
-                self.lookup_tag(url, tag)
-            elif not self.present and '-lts-' in tag:
-                tag = 'Ubuntu-lts-{}'.format(
-                    version.replace('~', '_'))
-                self.lookup_tag(url, tag)
+            # Handle the various tag form exceptions we have gained over the
+            # years.
+            tags = [tag]
+            if '-edge' in tag:
+                tags.append(tag.replace('-edge', ''))
+            if '-lts-' in tag:
+                tags.append('Ubuntu-lts-{}'.format(
+                    version.replace('~', '_')))
+            for tag in tags:
+                tag = 'refs/tags/' + tag
+                if tag in self._refs:
+                    self._present = True
+                    break
 
         cleave(self.__class__.__name__ + '.__init__')
 
-    def lookup_tag(self, url, tag):
-        center(self.__class__.__name__ + '.lookup_tag')
+    def cache_tags(self, url):
+        center(self.__class__.__name__ + '.cache_tags')
         cdebug('url     : {}'.format(url))
-        cdebug('tag     : {}'.format(tag))
 
+        refs = {}
         if url.startswith("git://git.launchpad.net"):
-            self.verifiable = True
+            self._verifiable = True
 
-            url = url.replace('git:', 'https:') + "/tag/?id={}".format(quote_plus(tag))
-            self.dbg_url = url
-            try:
-                # XXX: this should be a separate function which returns an opener
-                #      with proxy if the object is _external_.
-                opener = build_opener(ProxyHandler())
-                req = Request(url)
-                with opener.open(req) as resp:
-                    self._data = resp.read().decode('utf-8')
+            cmd = [ 'git', 'ls-remote', url ]
+            result = run(cmd, stdout=PIPE, stderr=PIPE)
+            if result.returncode != 0:
+                raise GitTagError(str(result.stderr))
 
-                self._present = not "<div class='error'>" in self._data
-
-            except (HTTPError, URLError) as e:
-                raise GitTagError(str(e))
+            for line in result.stdout.decode('utf-8').split('\n'):
+                if '\t' not in line:
+                    continue
+                (sha, ref) = line.split('\t')
+                refs[ref] = sha
 
         cleave(self.__class__.__name__ + '.lookup_tag')
+        return refs
+
+    @property
+    def verifiable(self):
+        return self._verifiable
 
     @property
     def present(self):
