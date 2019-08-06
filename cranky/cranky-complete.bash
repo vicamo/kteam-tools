a8928a8b9cd39b879f482d1c1953867d7d1b56fb
Juerg Haefliger <juergh@canonical.com>
2019-08-06 08:12:16 +0200

    cranky/cranky-complete-bash: Fix HANDLER and use shell-helper
    It's kernel handle, not handler. Also use the new shell-helper
    list-handles command rather than calling config.py directly.
    
    Acked-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>
    Signed-off-by: Juerg Haefliger <juergh@canonical.com>

--- cranky/cranky-complete.bash
+++ cranky/cranky-complete.bash
@@ -8,39 +8,39 @@
 _cranky() {
 	local cur first cmd opts
 	local cmds=(
-		"build-sources -h --help HANDLER"
-		"checkout -h --help -r --reference -d --dissociate HANDLER"
-		"chroot -h --help create-base create-session map-session run HANDLER"
-		"clone -h --help -r --reference -d --dissociate HANDLER"
+		"build-sources -h --help HANDLE"
+		"checkout -h --help -r --reference -d --dissociate HANDLE"
+		"chroot -h --help create-base create-session map-session run HANDLE"
+		"clone -h --help -r --reference -d --dissociate HANDLE"
 		"close -h --help -d --dry-run -c --include-config -s --skip-master"
 		"fdr -h --help -c --clean"
 		"fix -h --help"
 		"link-tb -h --help -d --dry-run -r --re-run -s --sru-cycle"
-		"list-routing -h --help -v --verbose HANDLER"
+		"list-routing -h --help -v --verbose HANDLE"
 		"open -h --help -d --dry-run -r --reuse-abi"
 		"rebase -h -r -b -l -d "
 		"reorder -h --help -d --dry-run"
 		"review -h --help -p --prev-dsc-dir -o --output"
-		"rmadison -h --help -a --show-all -e --show-extended HANDLER"
-		"spin -h --help -d --dry-run --devel HANDLER"
+		"rmadison -h --help -a --show-all -e --show-extended HANDLE"
+		"spin -h --help -d --dry-run --devel HANDLE"
 		"tag -h --help -v --verbose -f --force"
 		"test-build -h --help -a --arch -c --commit -d --dry-run -f --fail -p --purge -t --target"
 		"update-dependent -h --help"
 		"update-snap -h --help --dry-run -u --updates -t --no-tags"
 		"updateconfigs -h --help -c --clean"
 		"pull-source -h --help"
-		"dput-sources -h --help -f --force -e HANDLER"
+		"dput-sources -h --help -f --force -e HANDLE"
 		"promote-snap -h --help --dry-run --debug"
 	)
 	if [ "$COMP_CWORD" -eq 1 ]; then
 		for cmd in "${cmds[@]}"; do
-			opts+=$(_cranky_expand_handlers "${cmd%% *} ")
+			opts+=$(_cranky_expand_handles "${cmd%% *} ")
 		done
 	else
 		first="${COMP_WORDS[1]}"
 		for cmd in "${cmds[@]}"; do
 			if [ "$first" = "${cmd%% *}" ]; then
-				opts+=$(_cranky_expand_handlers "${cmd#* } ")
+				opts+=$(_cranky_expand_handles "${cmd#* } ")
 			fi
 		done
 	fi
@@ -51,19 +51,18 @@ _cranky() {
 	cur="${COMP_WORDS[COMP_CWORD]//:/\\:}"
 	mapfile -t COMPREPLY < <(compgen -W "$opts" -- "$cur")
 }
-complete -F _cranky cranky
 
-_cranky_expand_handlers() {
-	local handlers
+_cranky_expand_handles() {
+	local handles
 	case "$@" in
-		*HANDLER*)
-			handlers=$(_cranky_get_handlers)
+		*HANDLE*)
+			handles=$(_cranky_get_handles)
 			;;
 	esac
-	echo "${@//HANDLER/$handlers}"
+	echo "${@//HANDLE/$handles}"
 }
 
-_cranky_get_handlers() {
+_cranky_get_handles() {
 	# Cache the results here in the script to avoid the annoying
 	# delay caused by invoking the python interpreter and parsing
 	# the YAML file.
@@ -71,15 +70,15 @@ _cranky_get_handlers() {
 	local script_file="${BASH_SOURCE[0]}"
 	base_dir="$(dirname "$script_file")"
 	local cache_dir=~/.cache/cranky
-	local cache_file="$cache_dir/handlers"
-	local crl_config="$base_dir/crl/config.py"
+	local cache_file="$cache_dir/handles"
+	local shell_helper="$base_dir/cranky-shell-helper"
 	local yaml_file="$base_dir/../info/kernel-series.yaml"
 
-	if [ ! -e "$yaml_file" ] || [ ! -x "$crl_config" ]; then
+	if [ ! -e "$yaml_file" ] || [ ! -x "$shell_helper" ]; then
 		return
 	fi
 
-	if [ ! -e "$cache_file" ] ||
+	if [ ! -s "$cache_file" ] ||
 		   [ "$cache_file" -ot "$yaml_file" ] ||
 		   [ "$cache_file" -ot "$script_file" ]; then
 		# Cache is old, rebuild it:
@@ -87,8 +86,9 @@ _cranky_get_handlers() {
 			mkdir -p "$cache_dir"
 		fi
 		# Escape colons:
-		"$crl_config" list-handlers |
-			sed -e 's/:/\\\\&/g' > "$cache_file"
+		"$shell_helper" list-handles | sed -e 's/:/\\\\&/g' > "$cache_file"
 	fi
 	tr '\n' ' ' < "$cache_file"
 }
+
+complete -F _cranky cranky
