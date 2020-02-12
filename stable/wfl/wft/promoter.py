bc52ee03521da1ba48e40f3504260991081f08c7
Andy Whitcroft <apw@canonical.com>
2020-02-12 21:21:22 +0000

    swm: wft/promoter -- abbreviate missing artifacts
    When we are missing all of the nvidia artifacts we tend to spam the
    dashboard.  Only report the first couple and a count of additional ones.
    
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- stable/wfl/wft/promoter.py
+++ stable/wfl/wft/promoter.py
@@ -242,6 +242,8 @@ class Promoter(TaskHandler):
                     cdebug("missing {}".format(obj))
                     missing.append(obj)
             if len(missing) > 0:
+                if len(missing) > 3:
+                    missing = missing[0:2] + ['(+{} others)'.format(len(missing) - 2)]
                 s.task.reason = "Pending -- Nvidia objects not found -- {}".format(','.join(missing))
                 s.bug.refresh_at(datetime.now(timezone.utc) + timedelta(hours=1),
                     'polling for nvidia objects {}'.format(','.join(missing)))
