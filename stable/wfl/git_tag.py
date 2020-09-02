07c4bb0bb1edf300c37bc4614d77665a03877048
Andy Whitcroft <apw@canonical.com>
2020-09-02 21:31:24 +0100

    swm: git_tag -- add preliminary snap tag support
    Add support for checking for the source tip and tag for a snap.  Use the
    respository information to find the tag form and that form to find the
    tag associated with that tip.  From this we can extract the tip version
    for comparison.
    
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- stable/wfl/git_tag.py
+++ stable/wfl/git_tag.py
@@ -1,3 +1,5 @@
+import re
+
 from subprocess                 import Popen, PIPE, run
 
 from wfl.errors                 import ShankError
@@ -89,3 +91,121 @@ class GitTag():
     @property
     def present(self):
         return self._present
+
+
+# GitRemote
+#
+class GitRemote:
+
+    # __init__
+    #
+    def __init__(self, url):
+        center(self.__class__.__name__ + '.__init__')
+
+        self.url = url
+        self._refs = None
+        self._hashes = None
+
+        cleave(self.__class__.__name__ + '.__init__')
+
+    def cache_refs(self, url):
+        center(self.__class__.__name__ + '.cache_refs')
+        cdebug('url     : {}'.format(url))
+
+        cmd = ['git', 'ls-remote', url]
+        #print('  CMD: ' + ' '.join(cmd))
+        proc = Popen(cmd, stdout=PIPE)
+        refs = {}
+        hashes = {}
+        for entry in proc.stdout:
+            bits = entry.decode('utf-8').split()
+            if bits[1].endswith('^{}'):
+                bits[1] = bits[1][:-3]
+            refs.setdefault(bits[1], []).append(bits[0])
+            hashes.setdefault(bits[0], []).append(bits[1])
+        retcode = proc.wait()
+        if retcode != 0:
+            raise CalledProcessError(proc.returncode, proc.args,
+                proc.stdout, proc.stderr)
+
+        self._refs = refs
+        self._hashes = hashes
+        cleave(self.__class__.__name__ + '.cache_refs')
+
+    @property
+    def refs(self):
+        if self._refs is None:
+            self.cache_refs(self.url)
+        return self._refs
+
+    @property
+    def hashes(self):
+        if self._hashes is None:
+            self.cache_refs(self.url)
+        return self._hashes
+
+
+class GitTagsSnap:
+
+    def __init__(self, snap):
+        self.snap = snap
+
+        self._tip_tag = False
+        self._tip_version = False
+        self._tag_prefix = False
+
+        self.remote = None
+        if self.snap.repo is not None:
+            self.remote = GitRemote(self.snap.repo.url)
+
+    prefix_version_re = re.compile('^(.*-)([0-9.]+-[0-9._]+)$')
+    def analyse_tip(self):
+        if self.remote is None:
+            return None
+
+        # Mark this as attempted.
+        self._tip_tag = None
+        self._tip_version = None
+        self._tag_prefix = None
+
+        # Find the hash at the tip.
+        tip_name = 'refs/heads/' + self.snap.repo.branch
+        tip_hash = self.remote.refs.get(tip_name)
+        if tip_hash is None:
+            return
+        tip_hash = tip_hash[0]
+
+        # See if there are any other refs pointing to that.  If there
+        # is extract the tag prefix and current version from the tag.
+        for ref_name, ref_hash in self.remote.refs.items():
+            #print(tip_name, tip_hash, ref_name, ref_hash)
+            if ref_name != tip_name and tip_hash in ref_hash:
+                self._tip_tag = ref_name
+                pv_match = self.prefix_version_re.search(ref_name)
+                if pv_match is not None:
+                    self._tag_prefix = pv_match.group(1)
+                    self._tip_version = pv_match.group(2).replace('_', '~')
+
+    @property
+    def tag_prefix(self):
+        if self._tag_prefix is False:
+            self.analyse_tip()
+        return self._tag_prefix
+
+    @property
+    def tip_tag(self):
+        if self._tip_tag is False:
+            self.analyse_tip()
+        return self._tip_tag
+
+    @property
+    def tip_version(self):
+        if self._tip_version is False:
+            self.analyse_tip()
+        return self._tip_version
+
+    def lookup_version(self, version):
+        if self.tag_prefix is None:
+            return None
+        tag_name = self.tag_prefix + version.replace('~', '_')
+        return self.remote.refs.get(tag_name)
