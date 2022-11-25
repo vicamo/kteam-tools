e2bf34b4855312563cb610f2e836df823d596eea
Cory Todd <cory.todd@canonical.com>
2022-11-25 07:55:36 -0800

    cranky: crl: add __init__ with basic exports
    Add a new CrankyException class that so we don't have many versions of
    the same thing throughout the code. Add a path fixer for the ktl library
    since crl does depend on ktl in some places.
    
    Change-Id: Icb76e71d76293e23f6eee198194e210763e6c6f1
    Signed-off-by: Cory Todd <cory.todd@canonical.com>
    Reviewed-on: http://ckctreview.internal/r/c/kteam-tools/+/1259
    Acked-by: Jose Ogando Justo <jose.ogando@canonical.com>
    Signed-off-by: Cory Todd <cory.todd@canonical.com>

---
+++ cranky/crl/__init__.py
@@ -0,0 +1,11 @@
+# TODO fix library path once instead of repeating this in every file
+# currently, random import statements are altering the path so
+# transitive dependencies are prone to breaking in exciting ways.
+import os
+import sys
+
+sys.path.append(
+    os.path.realpath(
+        os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, "libs")
+    )
+)
---
+++ cranky/crl/exceptions.py
@@ -0,0 +1,2 @@
+class CrankyException(Exception):
+    pass
