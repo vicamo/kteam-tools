5f698bedb38d7aeaaea234b079a3ea6c9fa23bb6
Andy Whitcroft <apw@canonical.com>
2022-11-25 11:36:49 +0000

    swm: promote-to-as-proposed -- handle short as-proposed route list
    We only have as-proposed for the main archive.  Handle the as-proposed
    route list being short.
    
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- stable/wfl/wft/syn_promote_to_as_proposed.py
+++ stable/wfl/wft/syn_promote_to_as_proposed.py
@@ -55,11 +55,14 @@ class SynPromoteToAsProposed(TaskHandler):
                 status_ptp = s.bug.task_status('promote-to-proposed')
             if (status_ptp == 'Invalid' or
                     s.bug.debs.routing('as-proposed') is None or
+                    (s.bug.debs.built_in is not None and len(s.bug.debs.routing('as-proposed')) < s.bug.debs.built_in) or
                     s.bug.source.private):
                 if status_ptp == 'Invalid':
                     cinfo("promote-to-proposed: invalid, marking invalid")
                 elif s.bug.debs.routing('as-proposed') is None:
                     cinfo("no as-proposed route, marking invalid")
+                elif s.bug.debs.built_in is not None and len(s.bug.debs.routing('as-proposed')) < s.bug.debs.built_in:
+                    cinfo("no as-proposed route-entry, marking invalid")
                 elif s.bug.source.private:
                     cinfo("source marked private, marking invalid")
 
