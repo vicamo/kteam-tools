514f174eb1c5ceada8b933c72c7cfb85e6a21c94
Cory Todd <cory.todd@canonical.com>
2023-06-27 10:28:27 -0700

    ktl/signing_config: switch to production signing-config.yaml
    We no longer need to use staging.kernel.ubuntu.com. Drop the staging
    subdomain which simplifies our firewall rules.
    
    Signed-off-by: Cory Todd <cory.todd@canonical.com>

--- ktl/signing_config.py
+++ ktl/signing_config.py
@@ -26,7 +26,7 @@ class SigningConfigStreamLevel:
 
 class SigningConfig:
     def __init__(self, url=None, data=None, use_local=os.getenv("USE_LOCAL_SIGNING_CONFIG", False)):
-        self._url = 'https://staging.kernel.ubuntu.com/info/signing-config.yaml'
+        self._url = 'https://kernel.ubuntu.com/info/signing-config.yaml'
         try:
             import ckt_info
             _local = ckt_info.abspath("info/signing-config.yaml")
