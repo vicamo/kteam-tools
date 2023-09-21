0551b18a7d19051d87bf452527d55dd3b557909b
Andy Whitcroft <apw@canonical.com>
2023-09-21 12:39:35 +0100

    ktl/sru_report: add support for caching changelog lookups
    It is expensive to lookup changelog entries and they are immutable.  Add
    a new cache for these.  This is populated when the config "cache" is
    supplied.
    
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- ktl/sru_report.py
+++ ktl/sru_report.py
@@ -19,6 +19,16 @@ from logging                            import basicConfig, DEBUG
 from ktl.log                            import cdebug, cerror
 
 
+# BugEntry
+#
+class BugEntry:
+
+    def __init__(self, lp_bug):
+        self.title = lp_bug.title
+        self.tags = list(lp_bug.tags)
+        self.owner = lp_bug.owner.display_name
+
+
 # SruReport
 #
 class SruReport:
@@ -46,6 +56,12 @@ class SruReport:
         if 'debug' in self.cfg:
             log_format = "%(levelname)s - %(message)s"
             basicConfig(level=DEBUG, format=log_format)
+
+        self._changelog_cache = {}
+        self._changelog_cache_out = {}
+        if self.cfg.get("cache") is not None and os.path.exists(self.cfg["cache"]):
+            with open(self.cfg["cache"]) as cfd:
+                self._changelog_cache = json.load(cfd)
 
     def _dbg(self, system, msg):
         if 'debug' in self.cfg:
@@ -120,7 +136,7 @@ class SruReport:
 
     # _get_changelog_info
     #
-    def _get_changelog_info(self, url):
+    def _get_changelog_info_direct(self, url):
         '''Parse LP per-version/per-release page URL and return tuple (date, bugs)
         with a publishing date (time record) and a bug list string.'''
 
@@ -149,6 +165,33 @@ class SruReport:
                 bugnums.append(bug)
 
         return (date, bugnums)
+
+    def _get_changelog_info(self, url):
+        if url not in self._changelog_cache:
+            self._dbg('cache', "_get_changelog_info({}) (MISS)".format(url))
+            self._changelog_cache[url] = self._get_changelog_info_direct(url)
+        else:
+            self._dbg('cache', "_get_changelog_info({}) (HIT)".format(url))
+        date, bugs = self._changelog_cache[url]
+        if isinstance(date, list):
+            date = time.struct_time(date)
+        self._changelog_cache_out[url] = (date, bugs)
+        return date, bugs
+
+    def cache_save(self):
+        if "cache" in self.cfg:
+            with open(self.cfg["cache"] + ".new", "w") as cfd:
+                json.dump(self._changelog_cache_out, cfd, sort_keys=True, indent=4)
+                os.rename(self.cfg["cache"] + ".new", self.cfg["cache"])
+
+    _bug_cache = {}
+    def bug_data(self, bug):
+        if bug not in self._bug_cache:
+            self._dbg('cache', "bug_data({}) (MISS)".format(bug))
+            self._bug_cache[bug] = BugEntry(self.lp.bugs[bug])
+        else:
+            self._dbg('cache', "bug_data({}) (HIT)".format(bug))
+        return self._bug_cache[bug]
 
     # generate
     #
@@ -262,13 +305,14 @@ class SruReport:
                         results['releases'][rls][pkg]['age']  = age
                         results['releases'][rls][pkg]['bugs'] = {}
 
+                        self._dbg('cache', "rls={} package={}".format(rls, pkg))
                         for bug in bugs:
                             self._dbg('core', "        bug: '%s'" % (bug))
                             results['releases'][rls][pkg]['bugs'][bug] = {}
                             spammed_v1 = 'kernel-spammed-{}-{}'.format(rls, pkg)
                             spammed_v2 = spammed_v1 + "-v2"
                             try:
-                                lp_bug = self.lp.bugs[bug]
+                                lp_bug = self.bug_data(bug)
                                 if spammed_v1 in lp_bug.tags:
                                     key = rls
                                 else:
@@ -297,7 +341,7 @@ class SruReport:
                                     state = 'parent-validated'
 
                                 results['releases'][rls][pkg]['bugs'][bug]['title'] = lp_bug.title
-                                results['releases'][rls][pkg]['bugs'][bug]['owner'] = lp_bug.owner.display_name
+                                results['releases'][rls][pkg]['bugs'][bug]['owner'] = lp_bug.owner
                                 results['releases'][rls][pkg]['bugs'][bug]['state'] = state
 
                                 results['releases'][rls][pkg]['bugs'][bug]['tags']  = []
@@ -308,6 +352,8 @@ class SruReport:
                                 results['releases'][rls][pkg]['bugs'][bug]['owner'] = "unknown"
                                 results['releases'][rls][pkg]['bugs'][bug]['state'] = "unknown"
                                 results['releases'][rls][pkg]['bugs'][bug]['tags']  = []
+
+            self.cache_save()
 
             results['updated'] = datetime.utcnow().strftime("%A, %d. %B %Y %H:%M UTC")
             results['releases_order'] = self.requested_series
