a05ef671a1dcaa6777dfd422f385e8f480af6430
Juerg Haefliger <juerg.haefliger@canonical.com>
2023-11-08 12:50:23 +0100

    cranky: crl/config/test/test_config.py -- Fix coding style
    Let 'black' reformat the file and add it to the pre-commit whitelist.
    
    Signed-off-by: Juerg Haefliger <juerg.haefliger@canonical.com>

--- cranky/crl/tests/test_config.py
+++ cranky/crl/tests/test_config.py
@@ -2,17 +2,17 @@
 
 import os
 import sys
-from testfixtures               import TempDirectory
 import unittest
 import xdg
 
-sys.path.append(os.path.realpath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'libs')))
+from testfixtures import TempDirectory
 
-from crl.config                 import Config
+sys.path.append(os.path.realpath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, "libs")))
+
+from crl.config import Config  # noqa: E402
 
 
 class TestConfig(unittest.TestCase):
-
     data_yaml = """
     simple: data
     paths:
@@ -25,21 +25,21 @@ class TestConfig(unittest.TestCase):
         config = Config(data=self.data_yaml)
         self.assertNotEqual(config, None)
 
-        data = config.lookup('simple')
-        self.assertEqual(data, 'data')
+        data = config.lookup("simple")
+        self.assertEqual(data, "data")
 
     def test_config_data_paths_default_present(self):
         config = Config(data=self.data_yaml)
         self.assertNotEqual(config, None)
 
-        data = config.lookup('paths.default')
-        self.assertEqual(data, 'something')
+        data = config.lookup("paths.default")
+        self.assertEqual(data, "something")
 
     def test_config_data_simple2_absent(self):
         config = Config(data=self.data_yaml)
         self.assertNotEqual(config, None)
 
-        data = config.lookup('simple2')
+        data = config.lookup("simple2")
         self.assertIsNone(data)
 
     def test_config_data_dict_value_error(self):
@@ -47,77 +47,88 @@ class TestConfig(unittest.TestCase):
         self.assertNotEqual(config, None)
 
         with self.assertRaises(AttributeError):
-            data = config.lookup({})
-
+            _data = config.lookup({})
 
     def test_config_filename_present(self):
-        with TempDirectory() as d, unittest.mock.patch.dict(os.environ, {'HOME': '/non-existant'}):
-            d.write('cranky.conf', self.data_yaml.encode('utf-8'))
+        with TempDirectory() as d, unittest.mock.patch.dict(os.environ, {"HOME": "/non-existant"}):
+            d.write("cranky.conf", self.data_yaml.encode("utf-8"))
 
-            config = Config(filename=d.getpath('cranky.conf'))
+            config = Config(filename=d.getpath("cranky.conf"))
             self.assertNotEqual(config, None)
 
-            data = config.lookup('simple')
-            self.assertEqual(data, 'data')
+            data = config.lookup("simple")
+            self.assertEqual(data, "data")
 
     def test_config_filename_absent(self):
-        with TempDirectory() as d, unittest.mock.patch.dict(os.environ, {'HOME': '/non-existant'}):
-            config = Config(filename=d.getpath('cranky.conf'))
+        with TempDirectory() as d, unittest.mock.patch.dict(os.environ, {"HOME": "/non-existant"}):
+            config = Config(filename=d.getpath("cranky.conf"))
             self.assertNotEqual(config, None)
 
-            data = config.lookup('simple')
+            data = config.lookup("simple")
             self.assertIsNone(data)
 
     def test_config_xdg_config_home_cranky_dot_yaml(self):
         with TempDirectory() as d, unittest.mock.patch.object(xdg, "XDG_CONFIG_HOME", d.path):
-            d.write('cranky/cranky.yaml', self.data_yaml.encode('utf-8'))
+            d.write("cranky/cranky.yaml", self.data_yaml.encode("utf-8"))
 
             config = Config()
             self.assertNotEqual(config, None)
 
-            data = config.lookup('simple')
-            self.assertEqual(data, 'data')
+            data = config.lookup("simple")
+            self.assertEqual(data, "data")
 
     def test_config_home_cranky_dot_yaml(self):
-        with TempDirectory() as d, unittest.mock.patch.dict(os.environ, {'HOME': d.path}), unittest.mock.patch.object(xdg, "XDG_CONFIG_HOME", "/non-existant"):
-            d.write('.config/cranky/cranky.yaml', self.data_yaml.encode('utf-8'))
+        with (
+            TempDirectory() as d,
+            unittest.mock.patch.dict(os.environ, {"HOME": d.path}),
+            unittest.mock.patch.object(xdg, "XDG_CONFIG_HOME", "/non-existant"),
+        ):
+            d.write(".config/cranky/cranky.yaml", self.data_yaml.encode("utf-8"))
 
             config = Config()
             self.assertNotEqual(config, None)
 
-            data = config.lookup('simple')
-            self.assertEqual(data, 'data')
+            data = config.lookup("simple")
+            self.assertEqual(data, "data")
 
     def test_config_deprecated_home_cranky(self):
-        with TempDirectory() as d, unittest.mock.patch.dict(os.environ, {'HOME': d.path}), unittest.mock.patch.object(xdg, "XDG_CONFIG_HOME", "/non-existant"):
-            d.write('.cranky', self.data_yaml.encode('utf-8'))
+        with (
+            TempDirectory() as d,
+            unittest.mock.patch.dict(os.environ, {"HOME": d.path}),
+            unittest.mock.patch.object(xdg, "XDG_CONFIG_HOME", "/non-existant"),
+        ):
+            d.write(".cranky", self.data_yaml.encode("utf-8"))
 
             config = Config()
             self.assertNotEqual(config, None)
 
-            data = config.lookup('simple')
-            self.assertEqual(data, 'data')
+            data = config.lookup("simple")
+            self.assertEqual(data, "data")
 
     def test_config_deprecated_home_cranky_dot_yaml(self):
-        with TempDirectory() as d, unittest.mock.patch.dict(os.environ, {'HOME': d.path}), unittest.mock.patch.object(xdg, "XDG_CONFIG_HOME", "/non-existant"):
-            d.write('.cranky.yaml', self.data_yaml.encode('utf-8'))
+        with (
+            TempDirectory() as d,
+            unittest.mock.patch.dict(os.environ, {"HOME": d.path}),
+            unittest.mock.patch.object(xdg, "XDG_CONFIG_HOME", "/non-existant"),
+        ):
+            d.write(".cranky.yaml", self.data_yaml.encode("utf-8"))
 
             config = Config()
             self.assertNotEqual(config, None)
 
-            data = config.lookup('simple')
-            self.assertEqual(data, 'data')
+            data = config.lookup("simple")
+            self.assertEqual(data, "data")
 
     def test_config_xdg_config_home_override_empty(self):
         with TempDirectory() as d, unittest.mock.patch.object(xdg, "XDG_CONFIG_HOME", d.path):
-            d.write('cranky/cranky.yaml', self.data_yaml.encode('utf-8'))
+            d.write("cranky/cranky.yaml", self.data_yaml.encode("utf-8"))
 
-            config = Config(data='')
+            config = Config(data="")
             self.assertNotEqual(config, None)
 
-            data = config.lookup('simple')
+            data = config.lookup("simple")
             self.assertIsNone(data)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
