c3b1114835118bb7ff27e35c7b994c39899a2598
Agathe Porte <agathe.porte@canonical.com>
2023-11-23 16:47:12 +0100

    cranky: push-review: add new -f, --force option
    When re-pushing an already-uploaded crank, the `scp` call will
    correctly erase the existing files but the `git push` will fail to push
    the new tag.
    
    Add a new -f, --force option to allow to erase references on the remote
    git repository.
    
    Signed-off-by: Agathe Porte <agathe.porte@canonical.com>
    Acked-by: Thibault Ferrante <thibault.ferrante@canonical.com>
    Signed-off-by: Agathe Porte <agathe.porte@canonical.com>

--- cranky/cranky-complete.bash
+++ cranky/cranky-complete.bash
@@ -26,7 +26,7 @@ _cranky() {
 		"promote-snap -h --help --dry-run --debug"
 		"pull-source -h --help -n --no-verify"
 		"pull-sources  -h --help -n --no-verify -l --latest HANDLE"
-		"push-review -h --help -d --dry-run -s --sru-cycle"
+		"push-review -h --help -d --dry-run -s --sru-cycle -f --force"
 		"rebase -h -r -b -l -d --dry-run"
 		"reorder -h --help -d --dry-run"
 		"review -h --help -p --prev-dsc-dir -o --output"
