ff6bdc8d8fb3524c3ae5fe1607fd183b94df24d0
Stefan Bader <stefan.bader@canonical.com>
2023-12-05 18:06:14 +0100

    ktl/sru_cycle: Fix default local URL and add YAML dump support
    The local URL usage got broken when we moved the sru-cycle.yaml file.
    This now is part of a different repository. For simplicity require that
    the other repository is located in the same root as kteam-tools and is
    named 'kernel-versions'.
    
    Add a method to convert SruCycle and SruCycleSpinEntry into strings.
    When used on SruCycle this will generate the informational header and
    then the YAML definition of every cycle/spin entry from latest to
    oldest.
    
    To have the string conversion produce consistent output a sort key for
    cycle/spin names is added. This will first order ascending by date. The
    various cycle types get ordered by SRU, security, and devel. And within
    the same cycle the final order is spin number. A cycle entry without
    spin number is treated as if it had spin number 0.
    
    As a fix-up "cutoff-date" had to be added to the SruCycleSpinEntry
    parser to prevent it being lost.
    
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- ktl/sru_cycle.py
+++ ktl/sru_cycle.py
@@ -1,15 +1,53 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
 
 try:
     from urllib.request import urlopen
+    from urllib.error import HTTPError, URLError
 except ImportError:
     from urllib2 import urlopen
+    from urllib2 import HTTPError, URLError
 
 from datetime       import datetime
+from warnings       import warn
 import os
 import yaml
 
+
+KERNEL_VERSIONS_DIR = os.path.realpath(
+                        os.path.join(os.path.dirname(__file__), '..', '..',
+                                     'kernel-versions', 'info'))
+
+SRU_CYCLE_HEADER = '''# kernel SRU Cycle information file (YAML format)
+#
+# For all live SRU cycles this file contains a record.  That dictionary record
+# defines attributed of the SRU cycle such as its current release-data.
+#
+# File format:
+# The file en-toto is an unnamed dictionary containing a named series of Cycle
+# records (see below).  The name for a record is the official cycle name without
+# its respin number, for example 2018.05.21.
+#
+# Cycle record fields:
+#  * name (string)
+#       the name of this SRU Cycle
+#  * complete (bool)
+#       the cycle was completed and is archived
+#  * release-date (string)
+#       the release date of this SRU cycle; YYYY-MM-DD format
+#  * hold (bool)
+#       the cycle is on hold; infer kernel-block on all active packages
+#  * current (bool)
+#       indicates which sru cycle is the current cycle. the current field only
+#       needs to exist on the current cycle entry
+#  * cutoff-date (string)
+#       the date patches are no longer applied to the git repos for the given
+#       cycle
+#  * stream (integer)
+#       the stream which the cycle uses by default (1 if not present)
+#  * notes-link (string)
+#       a notes link; initially a jira card identifier KERN-1234
+'''
 
 class SruCycleSpinEntry:
     def __init__(self, spin, data=False, sc=None):
@@ -31,11 +69,13 @@ class SruCycleSpinEntry:
         sdate = data.get('start-date')
         if sdate is not None:
             sdate = datetime.strptime(sdate, '%Y-%m-%d').date()
+            self._start_date_explicit = True
         else:
             sdate = cycle
             if not sdate[0].isdigit():
                 sdate = sdate[1:]
             sdate = datetime.strptime(sdate, '%Y.%m.%d').date()
+            self._start_date_explicit = False
         self._start_date = sdate
 
         rdate = data.get('release-date')
@@ -45,6 +85,7 @@ class SruCycleSpinEntry:
 
         self._hold = data.get('hold', False)
         self._complete = data.get('complete', False)
+        self._cutoff_date = data.get('cutoff-date', None)
         self._stream = data.get('stream')
         self._notes_link = data.get('notes-link')
 
@@ -63,6 +104,10 @@ class SruCycleSpinEntry:
     @property
     def cycle(self):
         return self._cycle
+
+    @property
+    def cutoff_date(self):
+        return self._cutoff_date
 
     @property
     def start_date(self):
@@ -101,19 +146,61 @@ class SruCycleSpinEntry:
         return self._notes_link
 
     def __str__(self):
-        return "{} ({})".format(self.name, self.release_date)
+        s = "'{}':\n".format(self.name)
+        if self._hold:
+            s += "    hold: {}\n".format(str(self._hold).lower())
+        if self._cutoff_date:
+            s += "    cutoff-date: '{}'\n".format(self._cutoff_date)
+        if self._start_date_explicit:
+            s += "    start-date: '{}'\n".format(self._start_date)
+        if self._release_date:
+            s += "    release-date: '{}'\n".format(self._release_date)
+        if self._notes_link:
+            s += "    notes-link: {}\n".format(self._notes_link)
+        if self._stream:
+            s += "    stream: {}\n".format(self._stream)
+        if self._complete:
+            s += "    complete: {}\n".format(str(self._complete).lower())
+        return s
 
 
-# SruCycle
-#
+'''
+ Should be SruCycles
+'''
 class SruCycle:
-    #_url = 'https://git.launchpad.net/~canonical-kernel/+git/kteam-tools/plain/info/sru-cycle.yaml'
-    _url = 'https://kernel.ubuntu.com/info/sru-cycle.yaml'
-    _url_local = 'file://' + os.path.realpath(os.path.join(os.path.dirname(__file__),
-                                                           '..', 'info', 'sru-cycle.yaml'))
-    #_url = 'file:///home/apw/git2/kteam-tools/info/kernel-series.yaml'
-    #_url = 'file:///home/work/kteam-tools/info/kernel-series.yaml'
+    '''
+    Cycle Info accessor class (SRU and security cycles)
+
+    Environment variables can be used to change the data source being used:
+
+    - USE_LOCAL_SRU_CYCLE_YAML (bool, deprecated)
+      When set to 1 use the yaml file in the default local path
+    - SRU_CYCLE_USE=<key> with key being:
+        default|remote[:<url>] Read-only access via remote URL
+        local[:<path>]: RW access via local file (default location if path is not given)
+
+    For the default local path the 'kernel-versions' repository must be checked out with that
+    name in the same top level directory of kteam-tools.
+    '''
+    _cfg  = None
     _data = None
+
+    @classmethod
+    def key_name(cls, name):
+        '''
+        Returns a sorting key for a given cycle name. The goal is to sort by date and
+        for the same date have the SRU cycle name first, then the security cycle and
+        finally the devel cycle last.
+        '''
+        if name[:1].isalpha():
+            key = name[1:]
+            if name[:1] == "d":
+                key += "-3"
+            else:
+                key += "-2"
+        else:
+            key = name + "-1"
+        return key
 
     @classmethod
     def __load_once(cls, url):
@@ -124,16 +211,68 @@ class SruCycle:
                 data = data.decode('utf-8')
             cls._data = yaml.safe_load(data)
 
-    def __init__(self, url=None, data=None, use_local=os.getenv("USE_LOCAL_SRU_CYCLE_YAML", False)):
-        if data or url:
-            if url:
-                response = urlopen(url)
-                data = response.read()
-            if not isinstance(data, str):
-                data = data.decode('utf-8')
-            self._data = yaml.safe_load(data)
-        else:
-            self.__load_once(self._url_local if use_local else self._url)
+    def __init__(self, data_source=None):
+        '''
+        data_source (string): Optionally allows to override the source of data. Possible values:
+            default|remote[:<URL>]
+                Allows read-only access to the default remote URL.
+            local[:<path>]
+                Allows read-write access to a local file representation
+            direct:<string>
+                Allows to pass in yaml data directly. Since this is meant for testing
+                this is not available as a environment key.
+
+        '''
+        # This used to be a Launchpad git raw URL but that was unreliable. The web URL is
+        # updated every 2min and has a higher reliability.
+        _url = 'https://kernel.ubuntu.com/info/sru-cycle.yaml'
+
+        # The data source can be set from the environment but only if not directly done from
+        # the command line.
+        if data_source is None:
+            data_source = os.getnenv('SRU_CYCLE_USE', None)
+
+            if data_source and data_source.startswith('direct:'):
+                raise ValueError('The direct: method is not allowed via environment!')
+
+            # Deprecated mathod of overriding the data location
+            if data_source is None and os.getenv("USE_LOCAL_SRU_CYCLE_YAML", False):
+                warn('Use of USE_LOCAL_SRU_CYCLE_YAML is deprecated, use SRU_CYCLE_USE=local instead.')
+                data_source='local'
+
+        _data = None
+        if data_source is not None:
+            if data_source == 'default' or data_source == 'remote':
+                pass
+            elif data_source.startswith('remote:'):
+                _url = data_source.split(':', 1)[1]
+            elif data_source == 'local':
+                _url = 'file://' + os.path.join(KERNEL_VERSIONS_DIR, 'sru-cycle.yaml')
+            elif data_source.startswith('local:'):
+                _url = 'file://' + data_source.split(':', 1)[1]
+            elif data_source.startswith('direct:'):
+                _data = data_source.split(':', 1)[1]
+                if not isinstance(_data, str):
+                    _data = _data.decode('utf-8')
+                self._url = 'direct:'
+            else:
+                raise ValueError('{} is not a valid data source!'.format(data_source))
+
+        if not self._data:
+            self._url = _url
+            if not _data:
+                response = urlopen(_url)
+                _data = response.read()
+            if not isinstance(_data, str):
+                _data = _data.decode('utf-8')
+            self._data = yaml.safe_load(_data)
+
+    @property
+    def data_source(self):
+        '''
+        Returns the URL of the data source which was used to create the instance
+        '''
+        return self._url
 
     @property
     def cycles(self):
@@ -155,5 +294,16 @@ class SruCycle:
         if allow_missing is False and spin_entry.known is False:
             return None
         return spin_entry
+
+    def __str__(self):
+        '''
+        Returns the YAML representation of sru-cycle.yaml as a string (including
+        the informational header. This can be used to write a new file after any
+        changes.
+        '''
+        s = SRU_CYCLE_HEADER
+        for key in sorted(self._data.keys(), key=self.key_name, reverse=True):
+            s += "\n" + str(SruCycleSpinEntry(key, data=self._data[key], sc=self))
+        return s
 
 # vi:set ts=4 sw=4 expandtab:
