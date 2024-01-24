a60a8ad028574fa7c33cdd96c7aabb34e6a2d125
Juerg Haefliger <juerg.haefliger@canonical.com>
2024-01-24 18:00:39 +0100

    ktl/git: Recognize '(forward ported from ...)' line
    Extend the upstream source regex to also match:
    (forward ported from commit <sha1>...)
    
    While at it, tighten up the matching strings for cherry-picks and
    backports. We need to train people to do it properly and be consistent.
    
    Signed-off-by: Juerg Haefliger <juerg.haefliger@canonical.com>

--- ktl/git.py
+++ ktl/git.py
@@ -23,7 +23,7 @@ class Git:
     subject_rc = compile("^UBUNTU: (Ubuntu-.*)$")
     tag_rc     = compile("^Ubuntu-([a-z][^-]*-){0-2}(.*)$")
     source_rc = compile(
-            "\s*\[*\(*(?:cherry.*pick(?:ed)*|backport(?:ed)*)\sfrom\scommit\s([a-f0-9]+)\s*([^\s\]\)]*)\s*([^\s\]\)]*)"
+            "\s*\[*\(*(?:cherry picked|backported|forward ported)\sfrom\scommit\s([a-f0-9]+)\s*([^\s\]\)]*)\s*([^\s\]\)]*)"
         )
 
     log_results = {}
