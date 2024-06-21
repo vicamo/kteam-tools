f452bbdb34bdf0ed1c722fd969eeae8986f7514a
Agathe Porte <agathe.porte@canonical.com>
2024-06-21 18:56:23 +0200

    annotations-tools: run tests relative to cranky/
    The cranky/ directory is considered the base directory when running
    tests. This commit fixes the failing tests when running the following
    command:
    
    $ (cd cranky/ && python3 -m pytest)
    
    Signed-off-by: Agathe Porte <agathe.porte@canonical.com>

--- cranky/annotations-tools/tests/test_todo_note.py
+++ cranky/annotations-tools/tests/test_todo_note.py
@@ -7,8 +7,8 @@ from kconfig.annotations import Annotation, KConfig
 
 class TestTodoNote(unittest.TestCase):
     def test_todo(self):
-        a = Annotation("tests/data/annotations.todo-note.1")
-        c = KConfig("tests/data/config.todo-note.1")
+        a = Annotation("annotations-tools/tests/data/annotations.todo-note.1")
+        c = KConfig("annotations-tools/tests/data/config.todo-note.1")
         a.update(c, arch="amd64", flavour="gcp")
-        r = utils.load_json("tests/data/annotations.todo-note.1.result")
+        r = utils.load_json("annotations-tools/tests/data/annotations.todo-note.1.result")
         self.assertEqual(utils.to_dict(a), r)
