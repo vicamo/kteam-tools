a9d28fa5c8142dab7269b644d955e1987064e42a
Agathe Porte <agathe.porte@canonical.com>
2024-11-26 14:35:18 +0100

    cranky: push_refs: refine logs
    The summary display is quite erratic because of using multi-line strings
    in log messages.
    
    Introduce a dataclass to store what was done, refactor the code and
    display each performed action on one line. The new lines can be a bit
    long, but having multiline log lines or log lines that depend on the
    previous one to be understood is asking for trouble.
    
    Moreover, the new implementation can log dry runs and forced pushes.
    
    Before:
    
    INFO: Codename: noble
    INFO: Source:   linux
    ...
    INFO: Summary:
    INFO:   Pkg 'linux' in directory '.../linux-main'
       'Ubuntu-6.8.0-50.51' to 'git://.../linux/+git/noble'
    INFO:   Pkg 'linux' in directory '.../linux-main'
       'HEAD:master-next' to 'git://.../linux/+git/noble'
    INFO:   Pkg 'linux-meta' in directory '.../linux-meta'
       'Ubuntu-6.8.0-50.51' to 'git://.../linux-meta/+git/noble'
    INFO:   Pkg 'linux-meta' in directory '.../linux-meta'
       'HEAD:master' to 'git://.../linux-meta/+git/noble'
    INFO:   Pkg 'linux-signed' in directory '.../linux-signed'
       'Ubuntu-6.8.0-50.51' to 'git://.../linux-signed/+git/noble'
    INFO:   Pkg 'linux-signed' in directory '.../linux-signed'
       'HEAD:master' to 'git://.../linux-signed/+git/noble'
    INFO:   Pkg 'linux-restricted-modules' in directory '.../linux-lrm'
       'Ubuntu-6.8.0-50.51' to 'git://.../linux-restricted-modules/+git/noble'
    INFO:   Pkg 'linux-restricted-modules' in directory '.../linux-lrm'
       'HEAD:master' to 'git://.../linux-restricted-modules/+git/noble'
    
    After:
    
    INFO: push_refs: codename: noble, source: linux
    ...
    INFO: (dry run) linux (...): pushed 'Ubuntu-6.8.0-50.51' to 'git://.../linux/+git/noble'
    INFO: (dry run) linux (...): pushed 'HEAD:master-next' to 'git://.../linux/+git/noble'
    INFO: (dry run) linux-meta (...): pushed 'Ubuntu-6.8.0-50.51' to 'git://.../linux-meta/+git/noble'
    INFO: (dry run) linux-meta (...): pushed 'HEAD:master' to 'git://.../linux-meta/+git/noble'
    INFO: (dry run) linux-signed (...): pushed 'Ubuntu-6.8.0-50.51' to 'git://.../linux-signed/+git/noble'
    INFO: (dry run) linux-signed (...): pushed 'HEAD:master' to 'git://.../linux-signed/+git/noble'
    INFO: (dry run) linux-restricted-modules (...): pushed 'Ubuntu-6.8.0-50.51' to 'git://.../linux-restricted-modules/+git/noble'
    INFO: (dry run) linux-restricted-modules (...): pushed 'HEAD:master' to 'git://.../linux-restricted-modules/+git/noble'
    
    Signed-off-by: Agathe Porte <agathe.porte@canonical.com>
    Acked-by: Thibault Ferrante <thibault.ferrante@canonical.com>
    Signed-off-by: Thibault Ferrante <thibault.ferrante@canonical.com>

--- cranky/crankylib/push_refs.py
+++ cranky/crankylib/push_refs.py
@@ -2,39 +2,87 @@
 # push-refs subcommand
 #
 
+from dataclasses import dataclass
+from typing import Optional
+
 from crl.git import GitHandleSet
+
 from ktl.log import cerror, cnotice
 
 
-def push_refs(handle, cycle=None, dry_run=False):
+@dataclass
+class PushSummary:
+    """Represents a push performed by push_refs."""
+
+    package_name: str
+    directory: str
+    url: str
+    pushed: str
+    dry_run: bool = False
+    forced: bool = False
+
+    @classmethod
+    def from_git_handle(cls, gh, pushed: str, dry_run: bool = False, forced: bool = False):
+        return cls(
+            package_name=gh.package.name,
+            directory=gh.directory,
+            url=gh.get_url(),
+            pushed=pushed,
+            dry_run=dry_run,
+            forced=forced,
+        )
+
+    def __str__(self):
+        dry_run = "(dry run) " if self.dry_run else ""
+        forced = " (forced)" if self.forced else ""
+
+        return f"{dry_run}{self.package_name} ({self.directory}): pushed '{self.pushed}' to '{self.url}'{forced}"
+
+
+def push_current_tag(gh, dry_run: bool = False) -> Optional[PushSummary]:
+    current_tag = gh.get_current_tag()
+
+    if not current_tag:
+        cerror(f"No tag found for HEAD in {gh.directory}")
+        return
+
+    gh.push_ref(gh.remote, current_tag, dry_run=dry_run)
+    return PushSummary.from_git_handle(gh, pushed=current_tag, dry_run=dry_run)
+
+
+def push_head(gh, dry_run: bool = False) -> Optional[PushSummary]:
+    forced = False
+    rc = gh.push_ref(gh.remote, gh.remote_branch, "HEAD", dry_run=dry_run)
+
+    if rc != 0:
+        cerror(f"Unable to push HEAD to {gh.remote_branch}")
+        user_input = input("Do you want to push --force-with-lease? [y/N]")
+        if user_input == "y":
+            rc = gh.push_ref(gh.remote, gh.remote_branch, "HEAD", force=True, dry_run=dry_run)
+            forced = True
+        else:
+            return
+
+    return PushSummary.from_git_handle(gh, pushed="HEAD:" + gh.remote_branch, dry_run=dry_run, forced=forced)
+
+
+def push_refs(handle, cycle=None, dry_run: bool = False):
     """Push refs (HEADs and tags) that make up the set of kernel packages"""
     ghs = GitHandleSet(handle, cycle)
 
-    cnotice("Codename: {}".format(ghs.codename))
-    cnotice("Source:   {}".format(ghs.source.name))
+    cnotice(f"push_refs: codename: {ghs.codename}, source: {ghs.source.name}")
 
-    summary = []
+    summaries = []
     for gh in ghs:
-        pkg = gh.package
-        remote = gh.remote
-        rmt_branch = gh.remote_branch
-        local_ref = "HEAD"
-        current_tag = gh.get_current_tag()
-        if not current_tag:
-            cerror(f"No tag found for HEAD in {gh.directory}")
-        else:
-            gh.push_ref(remote, current_tag, dry_run=dry_run)
-            summary.append({"name": pkg.name, "dir": gh.directory, "pushed": current_tag, "url": gh.get_url()})
-        rc = gh.push_ref(remote, rmt_branch, local_ref, dry_run=dry_run)
-        if rc != 0:
-            cerror(f"Unable to push {local_ref} to {rmt_branch}")
-            user_input = input("Do you want to push --force-with-lease? [y/N]")
-            if user_input == "y":
-                rc = gh.push_ref(remote, rmt_branch, local_ref, force=True, dry_run=dry_run)
-        else:
-            summary.append({"name": pkg.name, "dir": gh.directory, "pushed": "HEAD:" + rmt_branch, "url": gh.get_url()})
+        tag_summary = push_current_tag(gh, dry_run)
+        if tag_summary is not None:
+            summaries.append(tag_summary)
 
-    # Print the summary
-    cnotice("Summary:")
-    for s in summary:
-        cnotice("  Pkg '{name}' in directory '{dir}'\n" "   '{pushed}' to '{url}'".format(**s))
+        head_summary = push_head(gh, dry_run)
+        if head_summary is not None:
+            summaries.append(head_summary)
+
+    # The logs can get clogged by the stdout of the git commands ran. Group summaries
+    # and print them at once.
+    for tag_summary in summaries:
+        cnotice(tag_summary)
